906ea66748c12b89a0309d06ca87f61a3badf7fe
sohyun127 <so405@naver.com>
2024-01-02 08:53:05 +0900

    [SETTING] : feature 모듈 / #2

---
+++ feature/build.gradle.kts
@@ -0,0 +1,36 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id("com.teamdontbe.dontbe.feature")
+    id("com.teamdontbe.dontbe.test")
+}
+
+android {
+    namespace = "com.teamdontbe.feature"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    // AndroidX
+    implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.fragment.ktx)
+    implementation(libs.splash.screen)
+
+    // Third Party
+    implementation(libs.coil.core)
+    implementation(libs.bundles.retrofit)
+}
