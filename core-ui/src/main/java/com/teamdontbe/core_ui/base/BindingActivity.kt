e7205e392b439375d1f621cec19488ba385f27e3
sohyun127 <so405@naver.com>
2024-01-02 08:04:30 +0900

    [SETTING] : core-ui 모듈 / #2

---
+++ core-ui/src/main/java/com/teamdontbe/core_ui/base/BindingActivity.kt
@@ -0,0 +1,30 @@
+package com.teamdontbe.core_ui.base
+
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.teamdontbe.core_ui.util.context.hideKeyboard
+
+abstract class BindingActivity<T : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int,
+) : AppCompatActivity() {
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.lifecycleOwner = this
+        initView()
+    }
+
+    protected abstract fun initView()
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        hideKeyboard(currentFocus ?: View(this))
+        return super.dispatchTouchEvent(ev)
+    }
+}
