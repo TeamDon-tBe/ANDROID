4e1e6ff3c0c151e07bf9fab01939b7562eef4fc1
chanu <cw8881@naver.com>
2024-01-04 18:58:44 +0900

    #6 [RENAME] : rename to notification, posting

---
+++ feature/src/main/java/com/teamdontbe/feature/notification/NotificationFragment.kt
@@ -0,0 +1,11 @@
+package com.teamdontbe.feature.notification
+
+import com.teamdontbe.core_ui.base.BindingFragment
+import com.teamdontbe.feature.R
+import com.teamdontbe.feature.databinding.FragmentNotificationBinding
+
+class NotificationFragment :
+    BindingFragment<FragmentNotificationBinding>(R.layout.fragment_notification) {
+    override fun initView() {
+    }
+}
---
+++ feature/src/main/java/com/teamdontbe/feature/posting/PostingFragment.kt
@@ -0,0 +1,10 @@
+package com.teamdontbe.feature.posting
+
+import com.teamdontbe.core_ui.base.BindingFragment
+import com.teamdontbe.feature.R
+import com.teamdontbe.feature.databinding.FragmentPostingBinding
+
+class PostingFragment : BindingFragment<FragmentPostingBinding>(R.layout.fragment_posting) {
+    override fun initView() {
+    }
+}
