b65e14a8caaeffbedd2a497f5c87495c78210c4a
chanu <cw8881@naver.com>
2024-01-05 01:28:11 +0900

    #6 [MOD] : MainActivity 코드 리뷰 반영, 코드 간결화

--- feature/src/main/java/com/teamdontbe/feature/MainActivity.kt
+++ feature/src/main/java/com/teamdontbe/feature/MainActivity.kt
@@ -15,29 +15,29 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     }
 
     private fun initMainBottomNavigation() {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
-        val navController = navHostFragment.findNavController()
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment).findNavController()
         binding.bnvMain.setupWithNavController(navController)
-
-        removeBadge(navController)
+        // NotificationFragment 진입할 때 badge를 없애는 함수
+        removeBadgeOnNotification(navController)
     }
 
-    private fun removeBadge(navController: NavController) {
+    private fun removeBadgeOnNotification(navController: NavController) {
+        val badgeDrawable = binding.bnvMain.getBadge(R.id.fragment_notification)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id == R.id.fragment_notification) {
-                val badgeDrawable = binding.bnvMain.getBadge(R.id.fragment_notification)
-                // notificationFragment 진입할때 없어짐
-                badgeDrawable?.isVisible = false
-                badgeDrawable?.clearNumber() // or badgeDrawable?.clearText()
+                badgeDrawable?.apply {
+                    isVisible = false
+                    clearNumber() // or badgeDrawable?.clearText()
+                }
             }
         }
     }
 
     private fun initMainBottomNaviBadge() {
-        val badge = binding.bnvMain.getOrCreateBadge(R.id.fragment_notification)
-        badge.isVisible = true
-        // 없애면 빨강색 dot만 존재
-        badge.number = 99 // or badge.text = "New"
+        binding.bnvMain.getOrCreateBadge(R.id.fragment_notification).apply {
+            isVisible = true
+            number = 99 // or badge.text = "New"
+        }
     }
 }
