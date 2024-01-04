b245ad3b240b34e9acd2a3a12526ee7a66b742fb
chanu <cw8881@naver.com>
2024-01-04 21:17:49 +0900

    #6 [FEAT] : MainActivity jetpack navigation 구현 및 nav graph 연결

--- feature/src/main/java/com/teamdontbe/feature/MainActivity.kt
+++ feature/src/main/java/com/teamdontbe/feature/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.teamdontbe.feature
 
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.teamdontbe.core_ui.base.BindingActivity
 import com.teamdontbe.feature.databinding.ActivityMainBinding
 
-
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
-    override fun initView() {
 
+    override fun initView() {
+        initMainBottomNavigation()
+    }
+
+    private fun initMainBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bnvMain.setupWithNavController(navController)
     }
 }
