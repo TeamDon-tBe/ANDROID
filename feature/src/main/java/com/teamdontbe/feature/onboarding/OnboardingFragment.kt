0a07791fb0466ec980a2a8b5ad77c4464d5327eb
Sohyun <so405@naver.com>
2024-01-09 16:30:52 +0900

    #27 [FEAT] : 온보딩 버튼 리스너 init

--- feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingFragment.kt
@@ -2,6 +2,7 @@ package com.teamdontbe.feature.onboarding
 
 import android.view.View
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.teamdontbe.core_ui.base.BindingFragment
@@ -16,6 +17,10 @@ class OnboardingFragment :
 
     override fun initView() {
         initOnboardingAdapter()
+        initBtnOnboardingNextClickListener()
+        initIvOnboardingBackClickListener()
+        initBtnOnboardingSkipClickListener()
+        initBtnOnboardingStartClickListener()
     }
 
     private val pageChangeCallback =
@@ -26,10 +31,10 @@ class OnboardingFragment :
                 positionOffsetPixels: Int,
             ) {
                 super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-                binding.btnOnboardingStart.isVisible = position == 3
+                binding.ivOnboardingBack.visibility =
+                    if (position == 0) View.INVISIBLE else View.VISIBLE
                 binding.btnOnboardingNext.isVisible = position != 3
-                binding.tvOnboardingSkip.isVisible = position != 3
-                binding.ivOnboardingBack.visibility = if (position == 0) View.INVISIBLE else View.VISIBLE
+                binding.btnOnboardingStart.isVisible = position == 3
             }
         }
 
@@ -38,18 +43,42 @@ class OnboardingFragment :
         binding.vpOnboarding.adapter = _onboardingAdapter
         binding.vpOnboarding.isUserInputEnabled = false
         binding.vpOnboarding.registerOnPageChangeCallback(pageChangeCallback)
+        TabLayoutMediator(binding.tabOnboardingIndicator, binding.vpOnboarding) { _, _ ->
+        }.attach()
+    }
+
+    private fun initBtnOnboardingNextClickListener() {
         binding.btnOnboardingNext.setOnClickListener {
             if (binding.vpOnboarding.currentItem + 1 < (_onboardingAdapter?.itemCount ?: 0)) {
                 binding.vpOnboarding.setCurrentItem(binding.vpOnboarding.currentItem + 1, true)
             }
         }
+    }
+
+    private fun initIvOnboardingBackClickListener() {
         binding.ivOnboardingBack.setOnClickListener {
             if (-1 < binding.vpOnboarding.currentItem - 1) {
                 binding.vpOnboarding.setCurrentItem(binding.vpOnboarding.currentItem - 1, true)
             }
         }
-        TabLayoutMediator(binding.tabOnboardingIndicator, binding.vpOnboarding) { _, _ ->
-        }.attach()
+    }
+
+    private fun initBtnOnboardingSkipClickListener() {
+        binding.tvOnboardingSkip.setOnClickListener {
+            navigateToHomeFragment()
+        }
+    }
+
+    private fun initBtnOnboardingStartClickListener()  {
+        binding.btnOnboardingStart.setOnClickListener {
+            navigateToHomeFragment()
+        }
+    }
+
+    private fun navigateToHomeFragment() {
+        findNavController().navigate(
+            R.id.action_onboarding_to_home,
+        )
     }
 
     override fun onDestroyView() {
