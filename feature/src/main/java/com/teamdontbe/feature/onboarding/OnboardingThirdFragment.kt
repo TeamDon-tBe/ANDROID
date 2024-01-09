efb582beca85a50d31901366e10ec0bb9e588dc4
Sohyun <so405@naver.com>
2024-01-09 16:30:01 +0900

    #27 [FEAT] : text view gradient 적용

--- feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingThirdFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingThirdFragment.kt
@@ -1,6 +1,8 @@
 package com.teamdontbe.feature.onboarding
 
 import com.teamdontbe.core_ui.base.BindingFragment
+import com.teamdontbe.core_ui.util.fragment.colorOf
+import com.teamdontbe.core_ui.view.setTextColorAsLinearGradient
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.ItemOnboardingThirdBinding
 import timber.log.Timber
@@ -9,5 +11,17 @@ class OnboardingThirdFragment :
     BindingFragment<ItemOnboardingThirdBinding>(R.layout.item_onboarding_third) {
     override fun initView() {
         Timber.d("온보딩 세번째")
+        setTextColorGradient()
+    }
+
+    private fun setTextColorGradient() {
+        binding.tvOnboardingThirdTitleGradient.setTextColorAsLinearGradient(
+            (
+                arrayOf(
+                    colorOf(R.color.black),
+                    colorOf(R.color.white),
+                )
+            ),
+        )
     }
 }
