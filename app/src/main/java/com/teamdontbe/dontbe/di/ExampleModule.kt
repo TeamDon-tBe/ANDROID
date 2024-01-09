54669385e58d21d21e8a8dbde12080ad43649b71
Sohyun <so405@naver.com>
2024-01-09 21:23:17 +0900

    #28 [FEAT] : di 의존성 추가

--- app/src/main/java/com/teamdontbe/dontbe/di/ExampleModule.kt
+++ app/src/main/java/com/teamdontbe/dontbe/di/ExampleModule.kt
@@ -4,6 +4,8 @@ import com.teamdontbe.data.datasource.ExampleDataSource
 import com.teamdontbe.data.repositoryimpl.ExampleRepositoryImpl
 import com.teamdontbe.data_remote.api.ExampleApiService
 import com.teamdontbe.data_remote.datasourceimpl.ExampleDataSourceImpl
+import com.teamdontbe.data_remote.datasourceimpl.ExamplePagingRepositoryImpl
+import com.teamdontbe.domain.repository.ExamplePagingRepository
 import com.teamdontbe.domain.repository.ExampleRepository
 import dagger.Binds
 import dagger.Module
@@ -13,23 +15,25 @@ import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
-
 @Module
 @InstallIn(SingletonComponent::class)
 object ExampleModule {
     @Provides
     @Singleton
-    fun provideSignService(@DontbeRetrofit retrofit: Retrofit): ExampleApiService =
-        retrofit.create(ExampleApiService::class.java)
+    fun provideSignService(
+        @DontbeRetrofit retrofit: Retrofit,
+    ): ExampleApiService = retrofit.create(ExampleApiService::class.java)
 
     @Module
     @InstallIn(SingletonComponent::class)
     interface RepositoryModule {
         @Binds
         @Singleton
-        fun bindsExampleRepository(
-            RepositoryImpl: ExampleRepositoryImpl,
-        ): ExampleRepository
+        fun bindsExampleRepository(RepositoryImpl: ExampleRepositoryImpl): ExampleRepository
+
+        @Binds
+        @Singleton
+        fun bindsExamplePagingRepository(RepositoryImpl: ExamplePagingRepositoryImpl): ExamplePagingRepository
     }
 
     @Module
