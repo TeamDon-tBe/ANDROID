1697aaff27387b7673930819465155a293f4223c
Sohyun <so405@naver.com>
2024-01-11 06:48:03 +0900

    #49 [FEAT] : 첫번째 아이템 padding 조정

--- feature/src/main/java/com/teamdontbe/feature/homedetail/HomeDetailFeedItemDecorator.kt
+++ feature/src/main/java/com/teamdontbe/feature/homedetail/HomeDetailFeedItemDecorator.kt
@@ -18,6 +18,12 @@ class HomeDetailFeedItemDecorator(val context: Context) : RecyclerView.ItemDecor
 
         if (position == 0) {
             outRect.top = context.pxToDp(0)
+            view.setPadding(
+                context.pxToDp(12),
+                context.pxToDp(16),
+                context.pxToDp(16),
+                context.pxToDp(20),
+            )
         } else {
             outRect.left = context.pxToDp(16)
             outRect.right = context.pxToDp(16)
