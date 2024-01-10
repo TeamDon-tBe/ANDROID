02a3a6cfa26eabea831f3f24493445d3970ab00d
Sohyun <so405@naver.com>
2024-01-11 06:24:57 +0900

    #49 [FEAT] : Home Detail 더미 데이터

--- feature/src/main/java/com/teamdontbe/feature/homedetail/HomeDetailFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/homedetail/HomeDetailFragment.kt
@@ -1,16 +1,16 @@
 package com.teamdontbe.feature.homedetail
 
 import android.os.Build
-import android.os.Bundle
-import androidx.navigation.fragment.findNavController
 import com.teamdontbe.core_ui.base.BindingFragment
 import com.teamdontbe.core_ui.util.fragment.statusBarColorOf
 import com.teamdontbe.feature.R
-import com.teamdontbe.feature.comment.CommentBottomSheet
 import com.teamdontbe.feature.databinding.FragmentHomeDetailBinding
 import com.teamdontbe.feature.home.Feed
+import com.teamdontbe.feature.home.HomeAdapter
 import com.teamdontbe.feature.home.HomeBottomSheet
 import com.teamdontbe.feature.home.HomeFragment
+import com.teamdontbe.feature.home.HomeFragment.Companion.KEY_FEED_DATA
+
 
 class HomeDetailFragment :
     BindingFragment<FragmentHomeDetailBinding>(R.layout.fragment_home_detail) {
