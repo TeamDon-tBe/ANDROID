6a374abe65741318f901f641ac0e6021357c207c
Sohyun <so405@naver.com>
2024-01-10 09:18:25 +0900

    #38 [FEAT] : Home 리스트 리사이클러뷰

--- feature/src/main/java/com/teamdontbe/feature/home/HomeFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/home/HomeFragment.kt
@@ -1,19 +1,68 @@
 package com.teamdontbe.feature.home
 
-import android.content.Intent
 import com.teamdontbe.core_ui.base.BindingFragment
+import com.teamdontbe.domain.entity.FeedEntity
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.FragmentHomeBinding
-import com.teamdontbe.feature.example.ExampleActivity
+import com.teamdontbe.feature.mypage.adapter.MyPageFeedItemDecorator
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun initView() {
-        initExampleBtnClickLister()
+        initHomeAdapter()
     }
 
-    private fun initExampleBtnClickLister() {
-        binding.btnHomeExample.setOnClickListener {
-            startActivity(Intent(requireContext(), ExampleActivity::class.java))
-        }
+    private fun initHomeAdapter() {
+        binding.rvHome.adapter =
+            HomeAdapter(click = { feedData, positoin ->
+            }).apply {
+                submitList(
+                    listOf(
+                        FeedEntity(
+                            3, "", "돈비돈비", false, false, 90, 200, 60, "돈비 사랑해", "2시간 전",
+                        ),
+                        FeedEntity(
+                            1, "", "먼지먼징", false, false, 100, 100, 30, "먼지는 성장기", "2시간 전",
+                        ),
+                        FeedEntity(
+                            2, "", "쿼카햄토리", false, false, 80, 50, 10, "쿼카햄은 짜파구리 요리사", "1시간 전",
+                        ),
+                        FeedEntity(
+                            4, "", "보람상쥐", false, false, 100, 100, 30, "진실의 방으로", "2시간 전",
+                        ),
+                        FeedEntity(
+                            5, "", "파주멀당", false, false, 80, 50, 10, "상인", "1시간 전",
+                        ),
+                        FeedEntity(
+                            6, "", "아 예예~", false, false, 90, 200, 60, "yes걸", "2시간 전",
+                        ),
+                        FeedEntity(
+                            6, "", "정언명렁-칸트", false, false, 90, 200, 60, "권나라", "2시간 전",
+                        ),
+                        FeedEntity(
+                            6, "", "희죽희죽", false, false, 90, 200, 60, "희죽", "2시간 전",
+                        ),
+                        FeedEntity(
+                            6, "", "쥬쥬", false, false, 90, 200, 60, "시크릿 공주", "2시간 전",
+                        ),
+                        FeedEntity(
+                            6, "", "상우야 변상해줘", false, false, 90, 200, 60, "새벽팟 등극", "2시간 전",
+                        ),
+                        FeedEntity(
+                            6, "", "미스터 다 빈", false, false, 90, 200, 60, "혹시 콩 좋아해?", "2시간 전",
+                        ),
+                        FeedEntity(
+                            6, "", "이승헌금좀주세요", false, false, 90, 200, 60, "새벽 런닝좌", "2시간 전",
+                        ),
+                        FeedEntity(
+                            6, "", "홍박사님을 아세요?", false, false, 90, 200, 60, "잠 좀 자!!", "2시간 전",
+                        ),
+                        FeedEntity(
+                            6, "", "무소유ㄴ", false, false, 90, 200, 60, "아쿠아리움 재밌었어?", "2시간 전",
+                        ),
+                    ),
+                )
+            }
+
+        binding.rvHome.addItemDecoration(MyPageFeedItemDecorator(requireContext()))
     }
 }
---
+++ feature/src/main/java/com/teamdontbe/feature/home/viewholder/HomeViewHolder.kt
@@ -0,0 +1,24 @@
+package com.teamdontbe.feature.home.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.teamdontbe.domain.entity.FeedEntity
+import com.teamdontbe.feature.databinding.ItemHomeFeedBinding
+import timber.log.Timber
+
+class HomeViewHolder(
+    private val binding: ItemHomeFeedBinding,
+    private val click: (FeedEntity, Int) -> Unit = { _, _ -> },
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(data: FeedEntity) {
+        with(binding) {
+            Timber.d(data.toString())
+            binding.tvHomeHeartNum.text = data.contentLikedNumber.toString()
+            binding.tvHomeCommentNum.text = data.commentNumber.toString()
+            feed = data
+            executePendingBindings()
+            binding.root.setOnClickListener {
+                click(data, adapterPosition)
+            }
+        }
+    }
+}
