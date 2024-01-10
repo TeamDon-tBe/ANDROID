c2c25123e5779db24f3a83673f2a23a6c8e1edf7
Sohyun <so405@naver.com>
2024-01-11 06:20:17 +0900

    #49 [FEAT] : Adatper 아이템 이동 구현

---
+++ feature/src/main/java/com/teamdontbe/feature/home/Feed.kt
@@ -0,0 +1,33 @@
+package com.teamdontbe.feature.home
+
+import android.os.Parcelable
+import com.teamdontbe.domain.entity.FeedEntity
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Feed(
+    val memberId: Int,
+    val memberProfileUrl: String,
+    val memberNickname: String,
+    val isLiked: Boolean,
+    val isGhost: Boolean,
+    val memberGhost: Int,
+    val contentLikedNumber: Int,
+    val commentNumber: Int,
+    val contentText: String,
+    val time: String,
+) : Parcelable {
+    fun toFeedEntity() =
+        FeedEntity(
+            memberId,
+            memberProfileUrl,
+            memberNickname,
+            isLiked,
+            isGhost,
+            memberGhost,
+            contentLikedNumber,
+            commentNumber,
+            contentText,
+            time,
+        )
+}
