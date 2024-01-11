7fe2fc775ae16e4cc5d5ba1acd7c22e198a40405
Sohyun <so405@naver.com>
2024-01-11 20:47:51 +0900

    #54 [FEAT] : commet bottom sheet edittext progress

--- feature/src/main/java/com/teamdontbe/feature/comment/CommentBottomSheet.kt
+++ feature/src/main/java/com/teamdontbe/feature/comment/CommentBottomSheet.kt
@@ -1,3 +1,50 @@
 package com.teamdontbe.feature.comment
 
-class CommentFragment : BInding
+import androidx.core.widget.doAfterTextChanged
+import com.teamdontbe.core_ui.base.BindingBottomSheetFragment
+import com.teamdontbe.core_ui.util.context.drawableOf
+import com.teamdontbe.feature.R
+import com.teamdontbe.feature.databinding.BottomsheetCommentBinding
+import com.teamdontbe.feature.util.Debouncer
+
+class CommentBottomSheet : BindingBottomSheetFragment<BottomsheetCommentBinding>(R.layout.bottomsheet_complaint_delete) {
+    private val commentDebouncer = Debouncer<String>()
+
+    override fun initView() {
+    }
+
+    private fun initEditText() {
+        binding.run {
+            etCommentContent.doAfterTextChanged {
+                when {
+                    etCommentContent.text.toString().length in 1..499 -> {
+                        layoutUploadBar.pbUploadBarInput.progressDrawable =
+                            context?.drawableOf(R.drawable.shape_primary_line_10_ring)
+                        layoutUploadBar.pbUploadBarInput.progress = etCommentContent.text.toString().length
+                        layoutUploadBar.btnUploadBarUpload.setImageResource(R.drawable.ic_uploading_activate)
+                        initUploadingBtnClickListener()
+                    }
+
+                    etCommentContent.text.toString().length >= 500 -> {
+                        layoutUploadBar.pbUploadBarInput.progressDrawable =
+                            context?.drawableOf(R.drawable.shape_error_line_10_ring)
+                        layoutUploadBar.pbUploadBarInput.progress = etCommentContent.text.toString().length
+                        layoutUploadBar.btnUploadBarUpload.setImageResource(R.drawable.ic_uploading_deactivate)
+                    }
+
+                    else -> {
+                        layoutUploadBar.pbUploadBarInput.progress = 0
+                        layoutUploadBar.btnUploadBarUpload.setImageResource(R.drawable.ic_uploading_deactivate)
+                    }
+                }
+                commentDebouncer.setDelay(binding.etCommentContent.text.toString(), 1000L) {
+                }
+            }
+        }
+    }
+
+    private fun initUploadingBtnClickListener() {
+        binding.layoutUploadBar.btnUploadBarUpload.setOnClickListener {
+        }
+    }
+}
