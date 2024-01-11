47936eb2b85620bee3bed4194af4272bfe7718a8
Eonji-sw <ejkim0625@gmail.com>
2024-01-12 05:34:25 +0900

    #51 [MOD] : 코드 리팩토링 및 폰트 수정

--- feature/src/main/java/com/teamdontbe/feature/notification/adapter/NotificationViewHolder.kt
+++ feature/src/main/java/com/teamdontbe/feature/notification/adapter/NotificationViewHolder.kt
@@ -2,8 +2,7 @@ package com.teamdontbe.feature.notification.adapter
 
 import android.text.Spannable
 import android.text.SpannableString
-import android.text.style.TextAppearanceSpan
-import androidx.core.content.ContextCompat.getString
+import android.text.style.StyleSpan
 import androidx.recyclerview.widget.RecyclerView
 import com.teamdontbe.domain.entity.NotiEntity
 import com.teamdontbe.feature.R
@@ -16,63 +15,39 @@ class NotificationViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(data: NotiEntity) {
         with(binding) {
-            when (data.notificationTriggerType) {
-                "contentLiked" -> {
-                    val a =
-                        data.triggerMemberNickname + root.context.getString(R.string.notification_feed_content_liked)
-                    val spannableText = SpannableString(a)
-                    spannableText.setSpan(
-                        TextAppearanceSpan(
-                            root.context,
-                            R.font.font_pretendard_semibold
-                        ),
-//                        StyleSpan(Typeface.BOLD),
-                        0,
-                        data.triggerMemberNickname.length,
-                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
-                    )
-                    tvNotificationFeed.text = spannableText
+            val spannableText =
+                when (data.notificationTriggerType) {
+                    "contentLiked" -> getSpannableText(data.triggerMemberNickname, R.string.notification_feed_content_liked)
+                    "comment" -> getSpannableText(data.triggerMemberNickname, R.string.notification_feed_comment)
+                    "commentLiked" -> getSpannableText(data.triggerMemberNickname, R.string.notification_feed_comment_liked)
+                    "actingContinue" -> getSpannableText(data.memberNickname, R.string.notification_feed_acting_continue)
+                    "beGhost" -> getSpannableText(data.memberNickname, R.string.notification_feed_be_ghost)
+                    "contentGhost" -> getSpannableText(data.memberNickname, R.string.notification_feed_content_ghost)
+                    "commentGhost" -> getSpannableText(data.memberNickname, R.string.notification_feed_comment_ghost)
+                    "userBan" -> getSpannableText(data.memberNickname, R.string.notification_feed_user_ban, 36)
+                    else -> {
+                        Timber.tag("noti").e("등록되지 않은 노티가 감지되었습니다 : ${data.notificationTriggerType}")
+                        SpannableString("")
+                    }
                 }
 
-                "comment" -> {
-                    tvNotificationFeed.text =
-                        data.triggerMemberNickname + root.context.getString(R.string.notification_feed_comment)
-                }
-
-                "commentLiked" -> {
-                    tvNotificationFeed.text =
-                        data.triggerMemberNickname + root.context.getString(R.string.notification_feed_comment_liked)
-                }
-
-                "actingContinue" -> {
-                    tvNotificationFeed.text =
-                        data.memberNickname + root.context.getString(R.string.notification_feed_acting_continue)
-                }
-
-                "beGhost" -> {
-                    tvNotificationFeed.text =
-                        data.memberNickname + root.context.getString(R.string.notification_feed_be_ghost)
-                }
-
-                "contentGhost" -> {
-                    tvNotificationFeed.text =
-                        data.memberNickname + root.context.getString(R.string.notification_feed_content_ghost)
-                }
-
-                "commentGhost" -> {
-                    tvNotificationFeed.text =
-                        data.memberNickname + root.context.getString(R.string.notification_feed_comment_ghost)
-                }
-
-                "userBan" -> {
-                    tvNotificationFeed.text =
-                        data.memberNickname + root.context.getString(R.string.notification_feed_user_ban)
-                }
-
-                else ->
-                    Timber.tag("noti")
-                        .e("등록되지 않은 노티가 감지되었습니다 : ${data.notificationTriggerType}")
-            }
+            tvNotificationFeed.text = spannableText
         }
+    }
+
+    private fun getSpannableText(
+        name: String,
+        resId: Int,
+        endIndex: Int = 0,
+    ): SpannableString {
+        val text = name + binding.root.context.getString(resId)
+        val spannableText = SpannableString(text)
+        spannableText.setSpan(
+            StyleSpan(R.font.font_pretendard_semibold),
+            0,
+            name.length + endIndex,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
+        )
+        return spannableText
     }
 }
