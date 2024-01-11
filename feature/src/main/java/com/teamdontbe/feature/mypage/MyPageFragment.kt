d2ab518c6411d9402b0b5433c4b9b392b395852a
chanu <cw8881@naver.com>
2024-01-12 07:45:51 +0900

    #32 [feat] : view pager 탭 레이아웃 적용

--- feature/src/main/java/com/teamdontbe/feature/mypage/MyPageFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/mypage/MyPageFragment.kt
@@ -1,10 +1,12 @@
 package com.teamdontbe.feature.mypage
 
 import androidx.fragment.app.viewModels
+import com.google.android.material.tabs.TabLayoutMediator
 import com.teamdontbe.core_ui.base.BindingFragment
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.FragmentMyPageBinding
 import com.teamdontbe.feature.example.ExampleViewModel
+import com.teamdontbe.feature.mypage.viewpager.MyPageVpAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,7 +15,39 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
 
     override fun initView() {
         viewModel.getRecyclerviewTest()
+        binding.pbMyPageInput.setProgress(40)
+
+        /*
+
+                progressBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                        // 프로그래스바의 진행률에 따라 TextView의 X 좌표를 조절
+                        val newX = progress.toFloat() / progressBar.max * progressBar.width
+                        moveTextView(newX)
+                    }
+
+                    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                        // 사용자가 터치를 시작할 때의 동작
+                    }
+
+                    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                        // 사용자가 터치를 끝낼 때의 동작
+                    }
+                })
+        */
+
 //        collectExample()
+
+        binding.vpMyPage.adapter = MyPageVpAdapter(this) // viewpager연동완료
+
+        val tabTitleArray = arrayOf(
+            POSTING,
+            COMMENT,
+        )
+
+        TabLayoutMediator(binding.tabMyPage, binding.vpMyPage) { tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
     }
 
     /*
@@ -36,4 +70,8 @@ class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_
             }.launchIn(lifecycleScope)
         }
     */
+    companion object {
+        const val POSTING = "게시글"
+        const val COMMENT = "답글"
+    }
 }
