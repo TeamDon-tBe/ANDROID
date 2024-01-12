7956f7f2acace170ebece4d33801cf07831e2161
chanu <cw8881@naver.com>
2024-01-13 06:20:58 +0900

    #32 [feat] : MyPageCommentAdapter 구현

---
+++ feature/src/main/java/com/teamdontbe/feature/mypage/comment/MyPageCommentAdapter.kt
@@ -0,0 +1,35 @@
+package com.teamdontbe.feature.mypage.comment
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.teamdontbe.core_ui.view.ItemDiffCallback
+import com.teamdontbe.domain.entity.FeedEntity
+import com.teamdontbe.feature.databinding.ItemHomeFeedBinding
+
+class MyPageCommentAdapter(
+    private val onClickKebabBtn: (FeedEntity) -> Unit,
+    private val onItemClicked: (FeedEntity) -> Unit,
+    context: Context,
+) :
+    ListAdapter<FeedEntity, MyPageCommentViewHolder>(ExampleDiffCallback) {
+    private val inflater by lazy { LayoutInflater.from(context) }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPageCommentViewHolder {
+        val binding = ItemHomeFeedBinding.inflate(inflater, parent, false)
+        return MyPageCommentViewHolder(binding, onClickKebabBtn, onItemClicked)
+    }
+
+    override fun onBindViewHolder(holder: MyPageCommentViewHolder, position: Int) {
+        holder.onBind(currentList[position])
+    }
+
+    companion object {
+        private val ExampleDiffCallback =
+            ItemDiffCallback<FeedEntity>(
+                onItemsTheSame = { old, new -> old.memberId == new.memberId },
+                onContentsTheSame = { old, new -> old == new },
+            )
+    }
+}
