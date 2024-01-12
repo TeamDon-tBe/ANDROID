17a24bc604c18598bb5e2b4828504d4196cc8cb8
chanu <cw8881@naver.com>
2024-01-13 04:41:06 +0900

    #32 [feat] : 프로그레스바 라벨 프로그레스 진행률에 따른 동적 이동 구현

--- feature/src/main/java/com/teamdontbe/feature/mypage/MyPageFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/mypage/MyPageFragment.kt
@@ -1,75 +1,75 @@
 package com.teamdontbe.feature.mypage
 
-import androidx.fragment.app.viewModels
+import android.content.res.Resources
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.tabs.TabLayoutMediator
 import com.teamdontbe.core_ui.base.BindingFragment
+import com.teamdontbe.core_ui.util.context.pxToDp
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.FragmentMyPageBinding
-import com.teamdontbe.feature.example.ExampleViewModel
-import com.teamdontbe.feature.mypage.viewpager.MyPageVpAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
-    private val viewModel by viewModels<ExampleViewModel>()
-
     override fun initView() {
-        viewModel.getRecyclerviewTest()
-        binding.pbMyPageInput.setProgress(40)
+        initCollapseContentScrimColorToBlack()
+        initMyPageProgressBarUI()
+        initMyPageTabLayout()
+    }
 
-        /*
+    private fun initCollapseContentScrimColorToBlack() {
+        binding.collapseMyPage.setContentScrimColor(requireContext().getColor(R.color.black))
+    }
 
-                progressBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-                    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                        // 프로그래스바의 진행률에 따라 TextView의 X 좌표를 조절
-                        val newX = progress.toFloat() / progressBar.max * progressBar.width
-                        moveTextView(newX)
-                    }
+    private fun initMyPageProgressBarUI() {
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+            updateProgressWithLabel(
+                binding.pbMyPageInput,
+                30,
+                binding.tvMyPageTransparencyPercentage,
+                Resources.getSystem().displayMetrics.widthPixels,
+            )
+        }
+    }
 
-                    override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                        // 사용자가 터치를 시작할 때의 동작
-                    }
+    private fun updateProgressWithLabel(
+        progressBar: ProgressBar,
+        progressStatus: Int,
+        progressLabelTextView: TextView,
+        maxX: Int,
+    ) {
+        progressBar.progress = progressStatus
 
-                    override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                        // 사용자가 터치를 끝낼 때의 동작
-                    }
-                })
-        */
+        val textViewX = (
+            (progressStatus * (progressBar.width - 2)) / progressBar.max -
+                requireContext().pxToDp(12)
+            ) - (progressLabelTextView.width / 2)
+        val finalX =
+            if (progressLabelTextView.width + textViewX > maxX) (maxX - progressLabelTextView.width - 16) else textViewX + 16 /*your margin*/
 
-//        collectExample()
+        progressLabelTextView.apply {
+            x = if (finalX < 0) 16.toFloat() else finalX.toFloat()
+            text = "-$progressStatus%"
+        }
+    }
 
-        binding.vpMyPage.adapter = MyPageVpAdapter(this) // viewpager연동완료
+    private fun initMyPageTabLayout() = with(binding) {
+        vpMyPage.adapter = MyPageVpAdapter(this@MyPageFragment)
 
         val tabTitleArray = arrayOf(
             POSTING,
             COMMENT,
         )
 
-        TabLayoutMediator(binding.tabMyPage, binding.vpMyPage) { tab, position ->
+        TabLayoutMediator(tabMyPage, vpMyPage) { tab, position ->
             tab.text = tabTitleArray[position]
         }.attach()
     }
 
-    /*
-        private fun collectExample() {
-            viewModel.getExample.flowWithLifecycle(lifecycle).onEach {
-                when (it) {
-                    is UiState.Loading -> {}
-                    is UiState.Success -> {
-                        binding.rvMyPageFeed.apply {
-                            adapter = MyPageFeedAdapter(requireContext()).apply {
-                                submitList(it.data)
-                            }
-                            addItemDecoration(MyPageFeedItemDecorator(requireContext()))
-                        }
-                    }
-
-                    is UiState.Empty -> {}
-                    is UiState.Failure -> toast("실패")
-                }
-            }.launchIn(lifecycleScope)
-        }
-    */
     companion object {
         const val POSTING = "게시글"
         const val COMMENT = "답글"
