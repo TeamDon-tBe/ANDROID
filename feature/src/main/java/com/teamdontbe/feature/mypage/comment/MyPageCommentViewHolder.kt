d285e1276b59cf80f384b49be3526a07d2696db5
chanu <cw8881@naver.com>
2024-01-13 06:20:46 +0900

    #32 [feat] : MyPageCommentViewHolder 구현

---
+++ feature/src/main/java/com/teamdontbe/feature/mypage/comment/MyPageCommentViewHolder.kt
@@ -0,0 +1,39 @@
+package com.teamdontbe.feature.mypage.comment
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.teamdontbe.domain.entity.FeedEntity
+import com.teamdontbe.feature.databinding.ItemHomeFeedBinding
+
+class MyPageCommentViewHolder(
+    private val binding: ItemHomeFeedBinding,
+    private val onClickKebabBtn: (FeedEntity) -> Unit,
+    private val onItemClicked: (FeedEntity) -> Unit,
+) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    private var item: FeedEntity? = null
+
+    init {
+        binding.root.setOnClickListener {
+            item?.let { onItemClicked(it) }
+        }
+        binding.btnHomeKebab.setOnClickListener {
+            item?.let { onClickKebabBtn(it) }
+        }
+    }
+
+    fun onBind(data: FeedEntity) = with(binding) {
+        setVisibility()
+        feed = data
+        item = data
+        executePendingBindings()
+    }
+
+    private fun ItemHomeFeedBinding.setVisibility() {
+        ivHomeGhostFillGreen.visibility = View.INVISIBLE
+        ivHomeLinePale.visibility = View.INVISIBLE
+        btnHomeComment.visibility = View.GONE
+        tvHomeCommentNum.visibility = View.GONE
+    }
+}
