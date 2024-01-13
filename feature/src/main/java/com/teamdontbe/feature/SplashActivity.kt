9addb35197e1235d0ac2920e040f6a72ae6ac74f
Eonji-sw <ejkim0625@gmail.com>
2024-01-14 07:28:47 +0900

    #75 [MOD] : 스플래쉬 이동 activity 변경

--- feature/src/main/java/com/teamdontbe/feature/SplashActivity.kt
+++ feature/src/main/java/com/teamdontbe/feature/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Handler
 import com.teamdontbe.core_ui.base.BindingActivity
 import com.teamdontbe.feature.databinding.ActivitySplashBinding
+import com.teamdontbe.feature.login.LoginActivity
 
 class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     override fun initView() {
@@ -14,7 +15,7 @@ class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_
         // 타이머가 끝나면 내부 실행
         Handler().postDelayed(
             Runnable {
-                startActivity(Intent(this, MainActivity::class.java))
+                startActivity(Intent(this, LoginActivity::class.java))
                 // 현재 액티비티 닫기
                 finish()
             },
