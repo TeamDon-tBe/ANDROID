bcec23c4fe15b2ae1567379f4ccbd48fb37e2e15
Sohyun <so405@naver.com>
2024-01-14 04:35:46 +0900

    #61 [UI] : home comment, detail concat

--- feature/src/main/java/com/teamdontbe/feature/homedetail/HomeDetailFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/homedetail/HomeDetailFragment.kt
@@ -2,11 +2,13 @@ package com.teamdontbe.feature.homedetail
 
 import android.os.Build
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
 import com.teamdontbe.core_ui.base.BindingFragment
 import com.teamdontbe.core_ui.util.fragment.statusBarColorOf
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.FragmentHomeDetailBinding
 import com.teamdontbe.feature.home.Feed
+import com.teamdontbe.feature.home.HomeAdapter
 import com.teamdontbe.feature.home.HomeBottomSheet
 import com.teamdontbe.feature.home.HomeFragment
 import com.teamdontbe.feature.home.HomeFragment.Companion.KEY_FEED_DATA
@@ -15,13 +17,13 @@ class HomeDetailFragment :
     BindingFragment<FragmentHomeDetailBinding>(R.layout.fragment_home_detail) {
     override fun initView() {
         statusBarColorOf(R.color.white)
-        initHomeDetailAdapter()
+        initHomeDetailCommentAdapter()
         initBackBtnClickListener()
     }
 
-    private fun initHomeDetailAdapter() {
-        binding.rvHomeDetail.adapter =
-            HomeDetailAdapter(onClickKebabBtn = { feedData, positoin ->
+    private fun initHomeDetailCommentAdapter() {
+        val commentAdapter =
+            HomeDetailCommentAdapter(onClickKebabBtn = { feedData, positoin ->
                 initBottomSheet()
             }).apply {
                 submitList(
@@ -36,6 +38,19 @@ class HomeDetailFragment :
                     ),
                 )
             }
+
+        val detailAdapter =
+            HomeAdapter(onClickKebabBtn = { feedData, positoin ->
+                initBottomSheet()
+            }).apply {
+                submitList(
+                    listOf(
+                        getHomeFeedDetailData()?.toFeedEntity(),
+                    ),
+                )
+            }
+
+        binding.rvHomeDetail.adapter = ConcatAdapter(detailAdapter, commentAdapter)
         binding.rvHomeDetail.addItemDecoration(HomeDetailFeedItemDecorator(requireContext()))
     }
 
