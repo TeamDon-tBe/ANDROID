cabc58f60f8b28d6d619b72f55d7d48e076bdacf
chanu <cw8881@naver.com>
2024-01-14 03:56:06 +0900

    #63 [add] : 인포 페이지 child 뷰페이저 프레그먼트 추가

---
+++ feature/src/main/java/com/teamdontbe/feature/mypage/transperencyinfo/TransparencyInfoThirdFragment.kt
@@ -0,0 +1,11 @@
+package com.teamdontbe.feature.mypage.transperencyinfo
+
+import com.teamdontbe.core_ui.base.BindingFragment
+import com.teamdontbe.feature.R
+import com.teamdontbe.feature.databinding.FragmentTransperencyInfo3Binding
+
+class TransparencyInfoThirdFragment :
+    BindingFragment<FragmentTransperencyInfo3Binding>(R.layout.fragment_transperency_info_3) {
+    override fun initView() {
+    }
+}
