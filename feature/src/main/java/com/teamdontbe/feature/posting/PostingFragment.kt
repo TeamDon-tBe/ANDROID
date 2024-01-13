b40c1ec00112c85b05a2048f14900a539499573c
Eonji-sw <ejkim0625@gmail.com>
2024-01-14 01:37:59 +0900

    #66 [MOD] : posting 애니메이션 코드 추가

--- feature/src/main/java/com/teamdontbe/feature/posting/PostingFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/posting/PostingFragment.kt
@@ -1,5 +1,6 @@
 package com.teamdontbe.feature.posting
 
+import android.view.animation.AnimationUtils
 import androidx.core.widget.doAfterTextChanged
 import androidx.navigation.fragment.findNavController
 import com.teamdontbe.core_ui.base.BindingFragment
@@ -16,8 +17,9 @@ class PostingFragment : BindingFragment<FragmentPostingBinding>(R.layout.fragmen
     override fun initView() {
         statusBarColorOf(R.color.white)
 
-//        val animation = AnimationUtils.loadAnimation(requireContext(), R.anim.anim_posting_fragment_from_left)
-//        view?.startAnimation(animation)
+        val animation =
+            AnimationUtils.loadAnimation(requireContext(), R.anim.anim_posting_fragment_from_left)
+        view?.startAnimation(animation)
 
         initEditText()
         initCancelBtnClickListener()
