4eedf71e7e8dff71d656bc8c42596b772b65d068
chanu <cw8881@naver.com>
2024-01-17 04:19:30 +0900

    #93 [chore] : 상수화

--- data-remote/src/main/java/com/teamdontbe/data_remote/api/MyPageUserProfileApiService.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/api/MyPageUserProfileApiService.kt
@@ -2,16 +2,19 @@ package com.teamdontbe.data_remote.api
 
 import com.teamdontbe.data.dto.BaseResponse
 import com.teamdontbe.data.dto.response.ResponseMyPageUserProfileDto
+import com.teamdontbe.data_remote.api.LoginApiService.Companion.API
+import com.teamdontbe.data_remote.api.LoginApiService.Companion.V1
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface MyPageUserProfileApiService {
     companion object {
         const val VIEW_MEMBER = "viewmember"
+        const val VIEW_MEMBER_ID = "viewmemberId"
     }
 
-    @GET("/${LoginApiService.API}/${LoginApiService.V1}/$VIEW_MEMBER/{viewmemberId}")
+    @GET("/$API/$V1/$VIEW_MEMBER/{$VIEW_MEMBER_ID}")
     suspend fun getMyPageUserProfile(
-        @Path("viewmemberId") viewMemberID: Int,
+        @Path(VIEW_MEMBER_ID) viewMemberID: Int,
     ): BaseResponse<ResponseMyPageUserProfileDto>
 }
