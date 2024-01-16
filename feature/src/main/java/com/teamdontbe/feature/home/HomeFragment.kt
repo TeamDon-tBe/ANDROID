4262445c0485f71e6971c9d88e4bf9cf40af52c5
Sohyun <so405@naver.com>
2024-01-16 19:54:56 +0900

    #85 [FEAT] : Home feed list api 연결

--- feature/src/main/java/com/teamdontbe/feature/home/HomeFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/home/HomeFragment.kt
@@ -1,19 +1,43 @@
 package com.teamdontbe.feature.home
 
 import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.teamdontbe.core_ui.base.BindingFragment
+import com.teamdontbe.core_ui.view.UiState
 import com.teamdontbe.domain.entity.FeedEntity
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.FragmentHomeBinding
 import com.teamdontbe.feature.mypage.adapter.MyPageFeedItemDecorator
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
+@AndroidEntryPoint
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private val homeViewModel by viewModels<HomeViewModel>()
+
     override fun initView() {
-        initHomeAdapter()
+        initObserve()
     }
 
-    private fun initHomeAdapter() {
+    private fun initObserve() {
+        homeViewModel.getFeedList.flowWithLifecycle(lifecycle).onEach {
+            when (it) {
+                is UiState.Loading -> Unit
+                is UiState.Success -> {
+                    initHomeAdapter(it.data)
+                }
+
+                is UiState.Empty -> Unit
+                is UiState.Failure -> Unit
+            }
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun initHomeAdapter(feedData: List<FeedEntity>) {
         binding.rvHome.adapter =
             HomeAdapter(onClickKebabBtn = { feedData, positoin ->
                 initBottomSheet()
@@ -33,52 +57,7 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
                     ),
                 )
             }).apply {
-                submitList(
-                    listOf(
-                        FeedEntity(
-                            3, "", "돈비돈비", false, false, 90, 200, 60, "돈비 사랑해", "2시간 전",
-                        ),
-                        FeedEntity(
-                            1, "", "먼지먼징", false, false, 100, 100, 30, "먼지는 성장기", "2시간 전",
-                        ),
-                        FeedEntity(
-                            2, "", "쿼카햄토리", false, false, 80, 50, 10, "쿼카햄은 짜파구리 요리사", "1시간 전",
-                        ),
-                        FeedEntity(
-                            4, "", "보람상쥐", false, false, 100, 100, 30, "진실의 방으로", "2시간 전",
-                        ),
-                        FeedEntity(
-                            5, "", "파주멀당", false, false, 80, 50, 10, "상인", "1시간 전",
-                        ),
-                        FeedEntity(
-                            6, "", "아 예예~", false, false, 90, 200, 60, "yes걸", "2시간 전",
-                        ),
-                        FeedEntity(
-                            6, "", "정언명렁-칸트", false, false, 90, 200, 60, "권나라", "2시간 전",
-                        ),
-                        FeedEntity(
-                            6, "", "희죽희죽", false, false, 90, 200, 60, "희죽", "2시간 전",
-                        ),
-                        FeedEntity(
-                            6, "", "쥬쥬", false, false, 90, 200, 60, "시크릿 공주", "2시간 전",
-                        ),
-                        FeedEntity(
-                            6, "", "상우야 변상해줘", false, false, 90, 200, 60, "새벽팟 등극", "2시간 전",
-                        ),
-                        FeedEntity(
-                            6, "", "미스터 다 빈", false, false, 90, 200, 60, "혹시 콩 좋아해?", "2시간 전",
-                        ),
-                        FeedEntity(
-                            6, "", "이승헌금좀주세요", false, false, 90, 200, 60, "새벽 런닝좌", "2시간 전",
-                        ),
-                        FeedEntity(
-                            6, "", "홍박사님을 아세요?", false, false, 90, 200, 60, "잠 좀 자!!", "2시간 전",
-                        ),
-                        FeedEntity(
-                            6, "", "무소유ㄴ", false, false, 90, 200, 60, "아쿠아리움 재밌었어?", "2시간 전",
-                        ),
-                    ),
-                )
+                submitList(feedData)
             }
 
         binding.rvHome.addItemDecoration(MyPageFeedItemDecorator(requireContext()))
