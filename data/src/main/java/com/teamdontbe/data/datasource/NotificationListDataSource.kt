d4627b0c52f227f3fab24d96b56bee3e7a488e1f
Eonji-sw <ejkim0625@gmail.com>
2024-01-17 03:12:09 +0900

    #94 [FEAT] : notification count api data source 구현

--- data/src/main/java/com/teamdontbe/data/datasource/NotificationListDataSource.kt
+++ data/src/main/java/com/teamdontbe/data/datasource/NotificationListDataSource.kt
@@ -1,8 +1,11 @@
 package com.teamdontbe.data.datasource
 
 import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.response.ResponseNotificationCountDto
 import com.teamdontbe.data.dto.response.ResponseNotificationListDto
 
 interface NotificationListDataSource {
     suspend fun getNotificationList(): BaseResponse<List<ResponseNotificationListDto>>
+
+    suspend fun getNotificationCount(): BaseResponse<ResponseNotificationCountDto>
 }
