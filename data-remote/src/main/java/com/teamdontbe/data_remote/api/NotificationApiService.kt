91dfc099435cc60ddfc376654a8f571bc02bd9d8
Eonji-sw <ejkim0625@gmail.com>
2024-01-17 01:09:31 +0900

    #87 [FEAT] : notification api service 구현

---
+++ data-remote/src/main/java/com/teamdontbe/data_remote/api/NotificationApiService.kt
@@ -0,0 +1,17 @@
+package com.teamdontbe.data_remote.api
+
+import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.response.ResponseNotificationListDto
+import retrofit2.http.GET
+
+interface NotificationApiService {
+    companion object {
+        const val API = "api"
+        const val V1 = "v1"
+        const val MEMBER = "member"
+        const val NOTIFICATION_ALL = "notification-all"
+    }
+
+    @GET("/$API/$V1/$MEMBER/$NOTIFICATION_ALL")
+    suspend fun getNotificationList(): BaseResponse<List<ResponseNotificationListDto>>
+}
