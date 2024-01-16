bf4cecec0de52c6cd0b5fcf784c14895ea2a1c2b
chanu <cw8881@naver.com>
2024-01-17 04:24:09 +0900

    #93 [chore] : 상수화

--- data-remote/src/main/java/com/teamdontbe/data_remote/api/MyPageUserProfileApiService.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/api/MyPageUserProfileApiService.kt
@@ -1,7 +1,10 @@
 package com.teamdontbe.data_remote.api
 
 import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.response.ResponseFeedDto
 import com.teamdontbe.data.dto.response.ResponseMyPageUserProfileDto
+import com.teamdontbe.data_remote.api.LoginApiService.Companion.API
+import com.teamdontbe.data_remote.api.LoginApiService.Companion.V1
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -9,6 +12,8 @@ interface MyPageUserProfileApiService {
     companion object {
         const val VIEW_MEMBER = "viewmember"
         const val VIEW_MEMBER_ID = "viewmemberId"
+        const val MEMBER = "member"
+        const val CONTENTS = "contents"
     }
 
     @GET("/$API/$V1/$VIEW_MEMBER/{$VIEW_MEMBER_ID}")
@@ -16,8 +21,8 @@ interface MyPageUserProfileApiService {
         @Path(VIEW_MEMBER_ID) viewMemberID: Int,
     ): BaseResponse<ResponseMyPageUserProfileDto>
 
-    @GET("/${LoginApiService.API}/${LoginApiService.V1}/$MEMBER/{viewmemberId}/$CONTENTS")
+    @GET("/$API/$V1/$MEMBER/{$VIEW_MEMBER_ID}/$CONTENTS")
     suspend fun getMyPageFeedList(
-        @Path("viewmemberId") viewMemberID: Int,
+        @Path(VIEW_MEMBER_ID) viewMemberID: Int,
     ): BaseResponse<List<ResponseFeedDto>>
 }
