c94aeea959b10540f0fed685010fc5be4d010d0d
Sohyun <so405@naver.com>
2024-01-16 19:51:17 +0900

    #85 [FEAT] : Home DataSourceImpl

---
+++ data-remote/src/main/java/com/teamdontbe/data_remote/datasourceimpl/HomeDataSourceImpl.kt
@@ -0,0 +1,17 @@
+package com.teamdontbe.data_remote.datasourceimpl
+
+import com.teamdontbe.data.datasource.HomeDataSource
+import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.response.ResponseFeedDto
+import com.teamdontbe.data_remote.api.HomeApiService
+import javax.inject.Inject
+
+class HomeDataSourceImpl
+    @Inject
+    constructor(
+        private val homeApiService: HomeApiService,
+    ) : HomeDataSource {
+        override suspend fun getFeedList(): BaseResponse<List<ResponseFeedDto>> {
+            return homeApiService.getFeedList()
+        }
+    }
