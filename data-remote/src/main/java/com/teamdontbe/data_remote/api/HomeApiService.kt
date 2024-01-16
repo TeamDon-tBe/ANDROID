b2b39efc6fde0aacec6d5b3499fece1b8d042ad0
Sohyun <so405@naver.com>
2024-01-17 01:23:45 +0900

    #89 [FEAT] : comment api service

--- data-remote/src/main/java/com/teamdontbe/data_remote/api/HomeApiService.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/api/HomeApiService.kt
@@ -1,6 +1,7 @@
 package com.teamdontbe.data_remote.api
 
 import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.response.ResponseCommentDto
 import com.teamdontbe.data.dto.response.ResponseFeedDto
 import com.teamdontbe.data_remote.api.LoginApiService.Companion.API
 import com.teamdontbe.data_remote.api.LoginApiService.Companion.V1
@@ -10,8 +11,12 @@ interface HomeApiService {
     companion object {
         const val CONTENT = "content"
         const val ALL = "all"
+        const val COMMENT = "comment"
     }
 
     @GET("/$API/$V1/$CONTENT/$ALL")
     suspend fun getFeedList(): BaseResponse<List<ResponseFeedDto>>
+
+    @GET("$API/$V1/$CONTENT/{contentId}/$COMMENT/$ALL")
+    suspend fun getCommentList(): BaseResponse<ResponseCommentDto>
 }
