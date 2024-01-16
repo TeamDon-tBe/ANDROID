7ffd063c1a1270f707f3ecdb57315f36eb153172
chanu <cw8881@naver.com>
2024-01-17 07:22:58 +0900

    #101 [feat] : MyPageRepositoryImpl  getMyPageCommentList 구현체 구현

--- data/src/main/java/com/teamdontbe/data/repositoryimpl/MyPageRepositoryImpl.kt
+++ data/src/main/java/com/teamdontbe/data/repositoryimpl/MyPageRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.teamdontbe.data.repositoryimpl
 
 import com.teamdontbe.data.datasource.MyPageDataSource
 import com.teamdontbe.domain.entity.FeedEntity
+import com.teamdontbe.domain.entity.MyPageCommentEntity
 import com.teamdontbe.domain.entity.MyPageUserProfileEntity
 import com.teamdontbe.domain.repository.MyPageRepository
 import kotlinx.coroutines.flow.Flow
@@ -10,12 +11,12 @@ import javax.inject.Inject
 
 class MyPageRepositoryImpl
 @Inject constructor(
-    private val myPageUserProfileDataSource: MyPageDataSource,
+    private val myPageDataSource: MyPageDataSource,
 ) : MyPageRepository {
     override suspend fun getMyPageUserProfile(viewMemberId: Int): Flow<MyPageUserProfileEntity?> {
         return flow {
             val result = kotlin.runCatching {
-                myPageUserProfileDataSource.getMyPageUserProfileSource(viewMemberId).data?.toMyPageUserProfileEntity()
+                myPageDataSource.getMyPageUserProfileSource(viewMemberId).data?.toMyPageUserProfileEntity()
             }
             emit(result.getOrDefault(MyPageUserProfileEntity(0, "", "", "", 0)))
         }
@@ -24,8 +25,21 @@ class MyPageRepositoryImpl
     override suspend fun getMyPageFeedList(viewMemberId: Int): Flow<List<FeedEntity>?> {
         return flow {
             val result = kotlin.runCatching {
-                myPageUserProfileDataSource.getMyPageUserFeedListSource(viewMemberId).data?.map {
+                myPageDataSource.getMyPageUserFeedListSource(viewMemberId).data?.map {
                     it.toFeedEntity()
+                }
+            }
+            emit(
+                result.getOrDefault(result.getOrDefault(emptyList())),
+            )
+        }
+    }
+
+    override suspend fun getMyPageCommentList(viewMemberId: Int): Flow<List<MyPageCommentEntity>?> {
+        return flow {
+            val result = kotlin.runCatching {
+                myPageDataSource.getMyPageUserCommentListSource(viewMemberId).data?.map {
+                    it.toMyPageCommentEntity()
                 }
             }
             emit(
