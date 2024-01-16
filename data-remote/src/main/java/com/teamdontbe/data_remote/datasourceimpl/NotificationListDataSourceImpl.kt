7d087102257cdbe38cc3afc1db52a595c954c0bc
Eonji-sw <ejkim0625@gmail.com>
2024-01-17 03:12:17 +0900

    #94 [FEAT] : notification count api data source impl 구현

--- data-remote/src/main/java/com/teamdontbe/data_remote/datasourceimpl/NotificationListDataSourceImpl.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/datasourceimpl/NotificationListDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.teamdontbe.data_remote.datasourceimpl
 
 import com.teamdontbe.data.datasource.NotificationListDataSource
 import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.response.ResponseNotificationCountDto
 import com.teamdontbe.data.dto.response.ResponseNotificationListDto
 import com.teamdontbe.data_remote.api.NotificationApiService
 import javax.inject.Inject
@@ -11,7 +12,9 @@ class NotificationListDataSourceImpl
     constructor(
         private val notificationApiService: NotificationApiService,
     ) : NotificationListDataSource {
-        override suspend fun getNotificationList(): BaseResponse<List<ResponseNotificationListDto>> {
-            return notificationApiService.getNotificationList()
-        }
+        override suspend fun getNotificationList(): BaseResponse<List<ResponseNotificationListDto>> =
+            notificationApiService.getNotificationList()
+
+        override suspend fun getNotificationCount(): BaseResponse<ResponseNotificationCountDto> =
+            notificationApiService.getNotificationCount()
     }
