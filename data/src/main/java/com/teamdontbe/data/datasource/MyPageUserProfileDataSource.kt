ac11e3a273dcec20cbfade00353eca0d18e31b5f
chanu <cw8881@naver.com>
2024-01-17 01:36:58 +0900

    #93 [mod] : getMyPageUserFeedListSource 함수 추가

--- data/src/main/java/com/teamdontbe/data/datasource/MyPageUserProfileDataSource.kt
+++ data/src/main/java/com/teamdontbe/data/datasource/MyPageUserProfileDataSource.kt
@@ -1,8 +1,11 @@
 package com.teamdontbe.data.datasource
 
 import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.response.ResponseMyPageFeedListDto
 import com.teamdontbe.data.dto.response.ResponseMyPageUserProfileDto
 
 interface MyPageUserProfileDataSource {
     suspend fun getMyPageUserProfileSource(viewMemberId: Int): BaseResponse<ResponseMyPageUserProfileDto>
+
+    suspend fun getMyPageUserFeedListSource(viewMemberId: Int): BaseResponse<ResponseMyPageFeedListDto>
 }
