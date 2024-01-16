bc4f70540f835cefade51236984ce09cf9595435
chanu <cw8881@naver.com>
2024-01-16 18:03:16 +0900

    #83 [add] : MyPageUserProfileApiService 추가

---
+++ data-remote/src/main/java/com/teamdontbe/data_remote/api/MyPageUserProfileApiService.kt
@@ -0,0 +1,18 @@
+package com.teamdontbe.data_remote.api
+
+import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.response.ResponseMyPageUserProfileDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface MyPageUserProfileApiService {
+    companion object {
+        const val VIEW_MEMBER = "viewmember"
+    }
+
+    //    /api/v1/viewmember/1
+    @GET("/${LoginApiService.API}/${LoginApiService.V1}/$VIEW_MEMBER/{viewmemberId}")
+    suspend fun getMyPageUserProfile(
+        @Path("viewmemberId") viewMemberID: Int,
+    ): BaseResponse<ResponseMyPageUserProfileDto>
+}
