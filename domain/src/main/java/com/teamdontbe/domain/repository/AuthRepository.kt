942ea0b236631dfb8a2b730483c9b7f939f7feea
Sohyun <so405@naver.com>
2024-01-16 16:13:31 +0900

    #81 [FEAT] : Auth Repository 헤더 빼기

--- domain/src/main/java/com/teamdontbe/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/teamdontbe/domain/repository/AuthRepository.kt
@@ -4,5 +4,5 @@ import com.teamdontbe.domain.entity.AuthEntity
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
-    suspend fun login(auth:String, requestLogin: String): Flow<AuthEntity?>
+    suspend fun login(requestLogin: String): Flow<AuthEntity?>
 }
