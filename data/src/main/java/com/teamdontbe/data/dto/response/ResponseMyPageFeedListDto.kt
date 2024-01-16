05c6c70d29daea9d4649530cd9c834fb98cc1dd8
chanu <cw8881@naver.com>
2024-01-17 01:35:26 +0900

    #93 [add] : ResponseMyPageFeedListDto 추가

---
+++ data/src/main/java/com/teamdontbe/data/dto/response/ResponseMyPageFeedListDto.kt
@@ -0,0 +1,28 @@
+package com.teamdontbe.data.dto.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseMyPageFeedListDto(
+    @SerialName("memberId")
+    val memberId: Int,
+    @SerialName("commentNumber")
+    val commentNumber: Int,
+    @SerialName("contentLikedNumber")
+    val contentLikedNumber: Int,
+    @SerialName("contentText")
+    val contentText: String,
+    @SerialName("isGhost")
+    val isGhost: Boolean,
+    @SerialName("isLiked")
+    val isLiked: Boolean,
+    @SerialName("memberGhost")
+    val memberGhost: Int,
+    @SerialName("memberNickname")
+    val memberNickname: String,
+    @SerialName("memberProfileUrl")
+    val memberProfileUrl: String,
+    @SerialName("time")
+    val time: String,
+)
