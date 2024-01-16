ec1e0f30b0903761e8d4e4d0d06279f64aa177ab
chanu <cw8881@naver.com>
2024-01-16 18:03:44 +0900

    #83 [chore] : dto 순서 변경

--- data/src/main/java/com/teamdontbe/data/dto/response/ResponseMyPageUserProfileDto.kt
+++ data/src/main/java/com/teamdontbe/data/dto/response/ResponseMyPageUserProfileDto.kt
@@ -5,14 +5,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseMyPageUserProfileDto(
-    @SerialName("memberGhost")
-    val memberGhost: Int,
     @SerialName("memberId")
     val memberId: Int,
-    @SerialName("memberIntro")
-    val memberIntro: String,
-    @SerialName("memberProfileUrl")
-    val memberProfileUrl: String,
     @SerialName("nickname")
     val nickname: String,
+    @SerialName("memberProfileUrl")
+    val memberProfileUrl: String,
+    @SerialName("memberIntro")
+    val memberIntro: String,
+    @SerialName("memberGhost")
+    val memberGhost: Int,
 )
