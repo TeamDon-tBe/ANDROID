b21ea66d203486da82910f665a4dbb742adc1c5e
Eonji-sw <ejkim0625@gmail.com>
2024-01-17 01:10:00 +0900

    #87 [FEAT] : notification list api repository 구현

---
+++ domain/src/main/java/com/teamdontbe/domain/repository/NotificationListRepository.kt
@@ -0,0 +1,8 @@
+package com.teamdontbe.domain.repository
+
+import com.teamdontbe.domain.entity.NotiEntity
+import kotlinx.coroutines.flow.Flow
+
+interface NotificationListRepository {
+    suspend fun getNotificationList(): Flow<List<NotiEntity>?>
+}
