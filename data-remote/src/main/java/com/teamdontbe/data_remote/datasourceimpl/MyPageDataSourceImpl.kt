2367b0a485a33cc0a7f9e4ed3a87ce017f420a30
chanu <cw8881@naver.com>
2024-01-17 07:17:32 +0900

    #101 [mod] : MyPageDataSourceImpl getCommentListSource 추가

--- data-remote/src/main/java/com/teamdontbe/data_remote/datasourceimpl/MyPageDataSourceImpl.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/datasourceimpl/MyPageDataSourceImpl.kt
@@ -3,17 +3,21 @@ package com.teamdontbe.data_remote.datasourceimpl
 import com.teamdontbe.data.datasource.MyPageDataSource
 import com.teamdontbe.data.dto.BaseResponse
 import com.teamdontbe.data.dto.response.ResponseFeedDto
+import com.teamdontbe.data.dto.response.ResponseMyPageCommentDto
 import com.teamdontbe.data.dto.response.ResponseMyPageUserProfileDto
 import com.teamdontbe.data_remote.api.MyPageApiService
 import javax.inject.Inject
 
 class MyPageDataSourceImpl @Inject constructor(
-    private val myPageUserProfileApiService: MyPageApiService,
+    private val myPageApiService: MyPageApiService,
 ) :
     MyPageDataSource {
     override suspend fun getMyPageUserProfileSource(viewMemberId: Int): BaseResponse<ResponseMyPageUserProfileDto> =
-        myPageUserProfileApiService.getMyPageUserProfile(viewMemberId)
+        myPageApiService.getMyPageUserProfile(viewMemberId)
 
     override suspend fun getMyPageUserFeedListSource(viewMemberId: Int): BaseResponse<List<ResponseFeedDto>> =
-        myPageUserProfileApiService.getMyPageFeedList(viewMemberId)
+        myPageApiService.getMyPageFeedList(viewMemberId)
+
+    override suspend fun getMyPageUserCommentListSource(viewMemberId: Int): BaseResponse<List<ResponseMyPageCommentDto>> =
+        myPageApiService.getMyPageCommentList(viewMemberId)
 }
