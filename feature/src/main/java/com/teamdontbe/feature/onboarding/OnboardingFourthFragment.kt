0321f009aec55dc9eb6df28f257543ef85589bf8
Eonji-sw <ejkim0625@gmail.com>
2024-01-16 22:50:37 +0900

    #86 [MOD] : click 인자 추가

--- feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingFourthFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingFourthFragment.kt
@@ -7,18 +7,24 @@ import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.ItemOnboardingFourthBinding
 import timber.log.Timber
 
-class OnboardingFourthFragment :
+class OnboardingFourthFragment(private val click: (String) -> Unit = { _ -> }) :
     BindingFragment<ItemOnboardingFourthBinding>(R.layout.item_onboarding_fourth) {
     override fun initView() {
         Timber.d("온보딩 네번째")
         binding.tvOnboardingFourthNickname.text = "돈비 사랑해"
         binding.ivOnboardingFourthProfile.setImageDrawable(drawableOf(R.drawable.img_posting_profile))
         hideKeyboard()
+        setIntroductionData()
     }
 
     private fun hideKeyboard() {
         binding.root.setOnClickListener {
             requireContext().hideKeyboard(binding.root)
         }
+    }
+
+    private fun setIntroductionData() {
+        Timber.d(binding.etOnboardingFourthIntroduction.text.toString())
+        click(binding.etOnboardingFourthIntroduction.text.toString())
     }
 }
