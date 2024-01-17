94009885920441d4a28a0e7f68da38f315b47b76
chanu <cw8881@naver.com>
2024-01-17 21:38:24 +0900

    #107 [add] : 도메인영역 닉네임 체크 레포지터리 추상체 추가

--- domain/src/main/java/com/teamdontbe/domain/repository/LoginRepository.kt
+++ domain/src/main/java/com/teamdontbe/domain/repository/LoginRepository.kt
@@ -5,4 +5,6 @@ import kotlinx.coroutines.flow.Flow
 
 interface LoginRepository {
     suspend fun postLogin(requestLogin: String): Flow<LoginEntity?>
+
+    suspend fun getNickNameDoubleCheck(nickName: String): Flow<String>
 }
