98d1050090f44d6b0f46b8a3a51e2a4633dc8ef9
chanu <cw8881@naver.com>
2024-01-17 21:36:50 +0900

    #107 [add] : LoginApiService에 닉네임 체크 api 추가

--- data-remote/src/main/java/com/teamdontbe/data_remote/api/LoginApiService.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/api/LoginApiService.kt
@@ -4,17 +4,26 @@ import com.teamdontbe.data.dto.BaseResponse
 import com.teamdontbe.data.dto.request.RequestLoginDto
 import com.teamdontbe.data.dto.response.ResponseLoginDto
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface LoginApiService {
     companion object {
         const val API = "api"
         const val V1 = "v1"
         const val AUTH = "auth"
+        const val NICKNAME_VALIDATION = "nickname-validation"
+        const val NICKNAME = "nickname"
     }
 
     @POST("/$API/$V1/$AUTH")
     suspend fun postLogin(
         @Body requestLogin: RequestLoginDto,
     ): BaseResponse<ResponseLoginDto>
+
+    @GET("/$API/$V1/$NICKNAME_VALIDATION")
+    suspend fun nickNameDoubleCheck(
+        @Query(NICKNAME) nickname: String,
+    ): BaseResponse<Unit>
 }
