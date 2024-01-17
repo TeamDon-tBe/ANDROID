e3412c51cbf381d8e5c38a16c9ed6f77666088cd
chanu <cw8881@naver.com>
2024-01-17 21:39:28 +0900

    #107 [feat] : 닉네임 중복 체크 로직 구현

--- feature/src/main/java/com/teamdontbe/feature/signup/SignUpProfileActivity.kt
+++ feature/src/main/java/com/teamdontbe/feature/signup/SignUpProfileActivity.kt
@@ -1,12 +1,17 @@
 package com.teamdontbe.feature.signup
 
 import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.teamdontbe.core_ui.base.BindingActivity
 import com.teamdontbe.core_ui.util.context.colorOf
+import com.teamdontbe.core_ui.view.UiState
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.ActivitySignUpProfileBinding
 import com.teamdontbe.feature.mypage.bottomsheet.MyPageBottomSheet.Companion.MY_PAGE_PROFILE
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class SignUpProfileActivity :
@@ -19,6 +24,7 @@ class SignUpProfileActivity :
         initMyPageProfileAppBarTitle()
         initUpdateErrorMessage()
         initDoubleBtnClickListener()
+        initMyPageStateObserve()
     }
 
     private fun initMyPageProfileAppBarTitle() {
@@ -40,13 +46,34 @@ class SignUpProfileActivity :
 
     private fun initDoubleBtnClickListener() {
         binding.btnSignUpProfileDoubleCheck.setOnClickListener {
-            val doubleCheckFlag = viewModel.isBtnSelected.value
-            val messageResId =
-                if (doubleCheckFlag == true) R.string.sign_up_profile_use_posssible else R.string.sign_up_profile_use_impossible
-            val textColorResId = if (doubleCheckFlag == true) R.color.primary else R.color.error
-
-            updateAgreeMessage(messageResId, textColorResId)
+            viewModel.getNickNameDoubleCheck(binding.etSignUpProfileNickname.text.toString())
         }
+    }
+
+    private fun updateErrorMessage(doubleCheck: Boolean) {
+        val messageResId =
+            if (doubleCheck) R.string.sign_up_profile_use_posssible else R.string.sign_up_profile_use_impossible
+        val textColorResId = if (doubleCheck) R.color.primary else R.color.error
+
+        updateAgreeMessage(messageResId, textColorResId)
+    }
+
+    private fun initMyPageStateObserve() {
+        viewModel.nickNameDoubleState.flowWithLifecycle(lifecycle).onEach {
+            when (it) {
+                is UiState.Loading -> Unit
+                is UiState.Success -> {
+                    if (it.data.isEmpty()) {
+                        updateErrorMessage(false)
+                    } else {
+                        updateErrorMessage(true)
+                    }
+                }
+
+                is UiState.Empty -> Unit
+                is UiState.Failure -> Unit
+            }
+        }.launchIn(lifecycleScope)
     }
 
     private fun updateAgreeMessage(messageResId: Int, textColorResId: Int) {
