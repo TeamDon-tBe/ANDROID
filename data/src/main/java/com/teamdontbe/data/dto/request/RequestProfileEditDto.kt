039dc08ad3f1becae39cce621580693b50a01326
chanu <cw8881@naver.com>
2024-01-18 06:50:08 +0900

    #113 [delete] : 필요없는 함수 제거

---
+++ data/src/main/java/com/teamdontbe/data/dto/request/RequestProfileEditDto.kt
@@ -0,0 +1,12 @@
+package com.teamdontbe.data.dto.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestProfileEditDto(
+    @SerialName("nickname") val nickName: String,
+    @SerialName("is_alarm_allowed") val isAlarmAllowed: Boolean,
+    @SerialName("member_intro") val memberIntro: String,
+    @SerialName("profile_url") val profileUrl: String,
+)
