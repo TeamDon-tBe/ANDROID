72c4f0739713fe64f9edcf2fcc74771d3ef463cc
Sohyun <so405@naver.com>
2024-01-17 20:00:57 +0900

    #105 [FEAT] : post liked api service

--- data-remote/src/main/java/com/teamdontbe/data_remote/api/HomeApiService.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/api/HomeApiService.kt
@@ -1,12 +1,15 @@
 package com.teamdontbe.data_remote.api
 
 import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.request.RequestFeedLikedDto
 import com.teamdontbe.data.dto.response.ResponseCommentDto
 import com.teamdontbe.data.dto.response.ResponseFeedDto
 import com.teamdontbe.data_remote.api.LoginApiService.Companion.API
 import com.teamdontbe.data_remote.api.LoginApiService.Companion.V1
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface HomeApiService {
@@ -16,6 +19,7 @@ interface HomeApiService {
         const val COMMENT = "comment"
         const val DETAIL = "detail"
         const val CONTENT_ID = "contentId"
+        const val LIKED = "liked"
     }
 
     @GET("/$API/$V1/$CONTENT/$ALL")
@@ -31,8 +35,14 @@ interface HomeApiService {
         @Path(value = CONTENT_ID) contentId: Int,
     ): BaseResponse<List<ResponseCommentDto>>
 
-    @DELETE("/$API/$V1/$CONTENT/{contentId}")
+    @DELETE("/$API/$V1/$CONTENT/{$CONTENT_ID}")
     suspend fun deleteFeed(
-        @Path(value = "contentId") contentId: Int,
+        @Path(value = CONTENT_ID) contentId: Int,
+    ): BaseResponse<Unit>
+
+    @POST("/$API/$V1/$CONTENT/{$CONTENT_ID}/$LIKED")
+    suspend fun postLiked(
+        @Path(value = CONTENT_ID) contentId: Int,
+        @Body request: RequestFeedLikedDto = RequestFeedLikedDto("Content"),
     ): BaseResponse<Unit>
 }
