7b4a83acf27d814d23e256ca07fb9b5d082e7551
chanu <cw8881@naver.com>
2024-01-18 06:41:01 +0900

    #113 [add] : patchUserProfile api 함수 추가

--- data-remote/src/main/java/com/teamdontbe/data_remote/api/LoginApiService.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/api/LoginApiService.kt
@@ -2,9 +2,11 @@ package com.teamdontbe.data_remote.api
 
 import com.teamdontbe.data.dto.BaseResponse
 import com.teamdontbe.data.dto.request.RequestLoginDto
+import com.teamdontbe.data.dto.request.RequestProfileEditDto
 import com.teamdontbe.data.dto.response.ResponseLoginDto
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Query
 
@@ -15,6 +17,7 @@ interface LoginApiService {
         const val AUTH = "auth"
         const val NICKNAME_VALIDATION = "nickname-validation"
         const val NICKNAME = "nickname"
+        const val USER_PROFILE = "user-profile"
     }
 
     @POST("/$API/$V1/$AUTH")
@@ -25,5 +28,10 @@ interface LoginApiService {
     @GET("/$API/$V1/$NICKNAME_VALIDATION")
     suspend fun nickNameDoubleCheck(
         @Query(NICKNAME) nickname: String,
+    ): BaseResponse<Unit>
+
+    @PATCH("/$API/$V1/$USER_PROFILE")
+    suspend fun patchUserProfile(
+        @Body requestProfileEdit: RequestProfileEditDto,
     ): BaseResponse<Unit>
 }
