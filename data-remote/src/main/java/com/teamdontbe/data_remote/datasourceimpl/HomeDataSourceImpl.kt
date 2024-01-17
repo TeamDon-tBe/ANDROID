98de0767dcc40376e15fa1b2a08466a6d116b459
Sohyun <so405@naver.com>
2024-01-18 04:17:21 +0900

    #109 [FEAT] : comment liked datasource impl

--- data-remote/src/main/java/com/teamdontbe/data_remote/datasourceimpl/HomeDataSourceImpl.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/datasourceimpl/HomeDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.teamdontbe.data_remote.datasourceimpl
 
 import com.teamdontbe.data.datasource.HomeDataSource
 import com.teamdontbe.data.dto.BaseResponse
+import com.teamdontbe.data.dto.request.RequestCommentLikedDto
 import com.teamdontbe.data.dto.request.RequestCommentPostingDto
 import com.teamdontbe.data.dto.response.ResponseCommentDto
 import com.teamdontbe.data.dto.response.ResponseFeedDto
@@ -46,5 +47,12 @@ class HomeDataSourceImpl
 
         override suspend fun deleteComment(commentId: Int): BaseResponse<Unit> {
             return homeApiService.deleteComment(commentId)
+        }
+
+        override suspend fun postCommentLiked(commentId: Int): BaseResponse<Unit> {
+            return homeApiService.postCommentLiked(
+                commentId,
+                RequestCommentLikedDto("test", "commentLiked"),
+            )
         }
     }
