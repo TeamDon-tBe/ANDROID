58e3b420145b3166078a025a766b8ed270c466bc
Sohyun <so405@naver.com>
2024-01-17 23:47:45 +0900

    #109 [FEAT] : Request Comment Posting Dto

---
+++ data/src/main/java/com/teamdontbe/data/dto/request/RequestCommentPostingDto.kt
@@ -0,0 +1,9 @@
+package com.teamdontbe.data.dto.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RequestCommentPostingDto(
+    @SerialName("commentText") val commentTex: String,
+)
