d288571310d3dd0bf19c60bda1b23e2d872d7dc3
Sohyun <so405@naver.com>
2024-01-18 23:07:01 +0900

    #144 [FEAT] : ripple 효과 제거

--- feature/src/main/java/com/teamdontbe/feature/SplashActivity.kt
+++ feature/src/main/java/com/teamdontbe/feature/SplashActivity.kt
@@ -8,7 +8,6 @@ import com.teamdontbe.core_ui.base.BindingActivity
 import com.teamdontbe.feature.databinding.ActivitySplashBinding
 import com.teamdontbe.feature.login.LoginActivity
 import com.teamdontbe.feature.login.LoginViewModel
-import com.teamdontbe.feature.signup.SignUpAgreeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,19 +22,20 @@ class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_
         // 타이머가 끝나면 내부 실행
         Handler().postDelayed(
             Runnable {
-                when {
-                    loginViewModel.getNickName().isNotBlank() -> {
-                        navigateTo<MainActivity>()
-                    }
-
-                    loginViewModel.getNickName().isBlank() -> {
-                        navigateTo<SignUpAgreeActivity>()
-                    }
-
-                    else -> {
-                        navigateTo<LoginActivity>()
-                    }
-                }
+                navigateTo<LoginActivity>()
+//                when {
+//                    loginViewModel.getNickName().isNotBlank() -> {
+//                        navigateTo<MainActivity>()
+//                    }
+//
+//                    loginViewModel.getNickName().isBlank() -> {
+//                        navigateTo<SignUpAgreeActivity>()
+//                    }
+//
+//                    else -> {
+//                        navigateTo<LoginActivity>()
+//                    }
+//                }
                 finish()
             },
             3000,
