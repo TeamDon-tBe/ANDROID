8e26fb81337515cd15b6a854c0e7c1a85048450b
chanu <cw8881@naver.com>
2024-01-18 23:09:28 +0900

    #137 [add] : 마이페이지 신고하기 기능 추가

--- feature/src/main/java/com/teamdontbe/feature/mypage/bottomsheet/MyPageAnotherUserBottomSheet.kt
+++ feature/src/main/java/com/teamdontbe/feature/mypage/bottomsheet/MyPageAnotherUserBottomSheet.kt
@@ -4,15 +4,29 @@ import com.teamdontbe.core_ui.base.BindingBottomSheetFragment
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.BottomsheetComplaintDeleteBinding
 
-class MyPageAnotherUserBottomSheet() :
+class MyPageAnotherUserBottomSheet :
     BindingBottomSheetFragment<BottomsheetComplaintDeleteBinding>(R.layout.bottomsheet_complaint_delete) {
     override fun initView() {
         initBottomSheetCloseClickListener()
+        initShowDialogClickListener()
     }
 
     private fun initBottomSheetCloseClickListener() {
         binding.ivComplaintDeleteClose.setOnClickListener {
             dismiss()
         }
+    }
+
+    private fun initShowDialogClickListener() {
+        binding.tvComplaintTitle.setOnClickListener {
+            MyPageDeclarDialogFragment().show(
+                childFragmentManager,
+                DECLARE_DIALOG,
+            )
+        }
+    }
+
+    companion object {
+        const val DECLARE_DIALOG = "declare"
     }
 }
---
+++ feature/src/main/java/com/teamdontbe/feature/mypage/bottomsheet/MyPageDeclarDialogFragment.kt
@@ -0,0 +1,42 @@
+package com.teamdontbe.feature.mypage.bottomsheet
+
+import android.content.Intent
+import android.net.Uri
+import com.teamdontbe.core_ui.base.BindingDialogFragment
+import com.teamdontbe.core_ui.util.context.dialogFragmentResize
+import com.teamdontbe.feature.R
+import com.teamdontbe.feature.databinding.FragmentDeleteWithTitleDialogBinding
+
+class MyPageDeclarDialogFragment :
+    BindingDialogFragment<FragmentDeleteWithTitleDialogBinding>(R.layout.fragment_delete_with_title_dialog) {
+    override fun initView() {
+        initCancelButtonClickListener()
+        initDeclareBtnClickListener()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        context?.dialogFragmentResize(this, 25.0f)
+    }
+
+    private fun initCancelButtonClickListener() {
+        binding.btnDeleteWithTitleDialogCancel.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    private fun initDeclareBtnClickListener() {
+        binding.btnDeleteWithTitleDialogDelete.setOnClickListener {
+            navigateToComplaintWeb()
+        }
+    }
+
+    private fun navigateToComplaintWeb() {
+        val urlIntentComplaint =
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://docs.google.com/forms/d/e/1FAIpQLSdjhidNLgk_99uHZ24pCIZX5V0Tn0CQ2sqpW4Aqahr3azQYyA/viewform"),
+            )
+        startActivity(urlIntentComplaint)
+    }
+}
