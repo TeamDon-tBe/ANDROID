539e81180daea94cd8b4ac8b1d7343c8b04012b7
Eonji-sw <ejkim0625@gmail.com>
2024-01-18 09:45:27 +0900

    #124 [MOD] : posting view model -> on boarding view model 분리

--- feature/src/main/java/com/teamdontbe/feature/posting/PostingViewModel.kt
+++ feature/src/main/java/com/teamdontbe/feature/posting/PostingViewModel.kt
@@ -1,15 +1,13 @@
 package com.teamdontbe.feature.posting
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.teamdontbe.core_ui.view.UiState
 import com.teamdontbe.domain.repository.PostingRepository
 import com.teamdontbe.domain.repository.UserInfoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,23 +19,16 @@ class PostingViewModel
         private val postingRepository: PostingRepository,
         private val userInfoRepository: UserInfoRepository,
     ) : ViewModel() {
-        private val _postPosting = MutableSharedFlow<UiState<Boolean>>()
-        val postPosting: SharedFlow<UiState<Boolean>> = _postPosting
-
-        private val _introduction = MutableLiveData("")
-        val introduction: LiveData<String> get() = _introduction
+        private val _postPosting = MutableStateFlow<UiState<Boolean>>(UiState.Empty)
+        val postPosting: StateFlow<UiState<Boolean>> = _postPosting
 
         fun posting(contentText: String) =
             viewModelScope.launch {
                 postingRepository.posting(contentText).collectLatest {
-                    _postPosting.emit(UiState.Success(it))
+                    _postPosting.value = UiState.Success(it)
                 }
-                _postPosting.emit(UiState.Loading)
+                _postPosting.value = UiState.Loading
             }
 
         fun getNickName() = userInfoRepository.getNickName()
-
-        fun setIntroduction(input: String) {
-            this._introduction.value = input
-        }
     }
