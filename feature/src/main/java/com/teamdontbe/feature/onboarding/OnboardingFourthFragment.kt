3d8beb5a8bffb6ff70a310b50d8fb73c3a367638
Eonji-sw <ejkim0625@gmail.com>
2024-01-18 10:46:39 +0900

    #124 [MOD] : onboarding patch user profile api 연결 코드 이동

--- feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingFourthFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingFourthFragment.kt
@@ -1,15 +1,11 @@
 package com.teamdontbe.feature.onboarding
 
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import com.teamdontbe.core_ui.base.BindingFragment
 import com.teamdontbe.core_ui.util.context.hideKeyboard
 import com.teamdontbe.core_ui.util.fragment.drawableOf
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.ItemOnboardingFourthBinding
-import com.teamdontbe.feature.posting.PostingViewModel
-import com.teamdontbe.feature.signup.SignUpAgreeActivity
-import com.teamdontbe.feature.signup.SignUpProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
@@ -17,7 +13,6 @@ import timber.log.Timber
 class OnboardingFourthFragment() :
     BindingFragment<ItemOnboardingFourthBinding>(R.layout.item_onboarding_fourth) {
     private val onboardingViewModel by activityViewModels<OnboardingViewModel>()
-    private val signUpProfileViewModel by viewModels<SignUpProfileViewModel>()
 
     override fun initView() {
         binding.vm = onboardingViewModel
@@ -25,13 +20,6 @@ class OnboardingFourthFragment() :
         binding.tvOnboardingFourthNickname.text = "돈비 사랑해"
         binding.ivOnboardingFourthProfile.setImageDrawable(drawableOf(R.drawable.img_posting_profile))
         hideKeyboard()
-
-        signUpProfileViewModel.patchUserProfileEdit(
-            inputNickName,
-            intent.getBooleanExtra(SignUpAgreeActivity.SIGN_UP_AGREE, false),
-            binding.etOnboardingFourthIntroduction.text.toString(),
-            "",
-        )
     }
 
     private fun hideKeyboard() {
