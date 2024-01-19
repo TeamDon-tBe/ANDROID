87dcd0c5b4f51f8cb50b4720a286e3bc75852768
Sohyun <so405@naver.com>
2024-01-19 21:53:49 +0900

    #161 [FIX] : edit text 닉네임 넣어서 수정

--- feature/src/main/java/com/teamdontbe/feature/SplashActivity.kt
+++ feature/src/main/java/com/teamdontbe/feature/SplashActivity.kt
@@ -6,7 +6,6 @@ import android.os.Handler
 import androidx.activity.viewModels
 import com.teamdontbe.core_ui.base.BindingActivity
 import com.teamdontbe.feature.databinding.ActivitySplashBinding
-import com.teamdontbe.feature.login.LoginActivity
 import com.teamdontbe.feature.login.LoginViewModel
 import com.teamdontbe.feature.signup.SignUpAgreeActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,22 +22,23 @@ class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_
         // 타이머가 끝나면 내부 실행
         Handler().postDelayed(
             Runnable {
-                when {
-                    loginViewModel.getNickName().isNotBlank() && loginViewModel.checkLogin() -> {
-                        navigateTo<MainActivity>()
-                    }
-
-                    loginViewModel.getNickName().isBlank() &&
-                        loginViewModel.getAccessToken()
-                            .isNotBlank()
-                    -> {
-                        navigateTo<SignUpAgreeActivity>()
-                    }
-
-                    else -> {
-                        navigateTo<LoginActivity>()
-                    }
-                }
+                navigateTo<SignUpAgreeActivity>()
+//                when {
+//                    loginViewModel.getNickName().isNotBlank() && loginViewModel.checkLogin() -> {
+//                        navigateTo<MainActivity>()
+//                    }
+//
+//                    loginViewModel.getNickName().isBlank() &&
+//                        loginViewModel.getAccessToken()
+//                            .isNotBlank()
+//                    -> {
+//                        navigateTo<SignUpAgreeActivity>()
+//                    }
+//
+//                    else -> {
+//                        navigateTo<LoginActivity>()
+//                    }
+//                }
                 finish()
             },
             3000,
