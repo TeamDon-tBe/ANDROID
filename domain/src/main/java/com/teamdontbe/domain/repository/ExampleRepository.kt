ae2c0ef128d6d22a856e18e6338f2cb0669d4d10
Eonji-sw <ejkim0625@gmail.com>
2024-01-19 21:37:42 +0900

    #160 [DEL] : 안쓰는 파일 삭제

--- domain/src/main/java/com/teamdontbe/domain/repository/ExampleRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.teamdontbe.domain.repository
-
-import com.teamdontbe.domain.entity.UserEntity
-import kotlinx.coroutines.flow.Flow
-
-interface ExampleRepository {
-    suspend fun getExample(page: Int): Flow<List<UserEntity>>
-}
