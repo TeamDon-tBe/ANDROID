0e4949da8ea6f44a57596490574d4b2c262fbe67
sohyun127 <so405@naver.com>
2024-02-08 02:29:56 +0900

    #174 [REFACTOR] : home viewholder 함수화

--- feature/src/main/java/com/teamdontbe/feature/home/viewholder/HomeViewHolder.kt
+++ feature/src/main/java/com/teamdontbe/feature/home/viewholder/HomeViewHolder.kt
@@ -1,50 +1,52 @@
 package com.teamdontbe.feature.home.viewholder
 
 import android.graphics.Color
-import android.view.View
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
 import com.teamdontbe.domain.entity.FeedEntity
-import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.ItemHomeFeedBinding
-import com.teamdontbe.feature.util.CalculateTime
 import com.teamdontbe.feature.util.Transparent
-import timber.log.Timber
 
 class HomeViewHolder(
     private val binding: ItemHomeFeedBinding,
-    private val onClickKebabBtn: (FeedEntity, Int) -> Unit = { _, _ -> },
-    private val onClickToNavigateToHomeDetail: (FeedEntity, Int) -> Unit = { _, _ -> },
-    private val onClickLikedBtn: (Int, Boolean) -> Unit = { _, _ -> },
-    private val onClickUserProfileBtn: (FeedEntity, Int) -> Unit = { _, _ -> },
-    private val onClickTransparentBtn: (FeedEntity, Int) -> Unit = { _, _ -> },
     private val userId: Int,
+    private val onClickToNavigateToHomeDetail: (FeedEntity) -> Unit,
+    private val onClickLikedBtn: (Int, Boolean) -> Unit,
+    private val onClickUserProfileBtn: (FeedEntity) -> Unit,
+    private val onClickKebabBtn: (FeedEntity, Int) -> Unit,
+    private val onClickTransparentBtn: (FeedEntity) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(data: FeedEntity) {
         with(binding) {
-            ivHomeProfile.load(R.drawable.ic_sign_up_profile_person)
-            if (data.memberId == userId) {
-                ivHomeGhostFillGreen.visibility = View.INVISIBLE
-                ivHomeLinePale.visibility = View.INVISIBLE
-            } else {
-                ivHomeGhostFillGreen.visibility = View.VISIBLE
-                ivHomeLinePale.visibility = View.VISIBLE
-            }
-
-            if (data.time.isNotEmpty()) {
-                tvHomeFeedTransparency.text = "투명도 ${data.memberGhost}% · ${
-                    CalculateTime(binding.root.context).getCalculateTime(data.time)
-                }"
-            }
-            btnHomeHeart.isSelected = data.isLiked
             feed = data
             executePendingBindings()
-            btnHomeKebab.setOnClickListener {
-                onClickKebabBtn(data, bindingAdapterPosition)
-            }
+
+
+            if (data.isGhost) setFeedTransparent(-85) else setFeedTransparent(data.memberGhost)
+
+            ivHomeGhostFillGreen.isVisible = data.memberId == userId
+            ivHomeLinePale.isVisible = data.memberId == userId
+            btnHomeHeart.isSelected = data.isLiked
+
             root.setOnClickListener {
-                onClickToNavigateToHomeDetail(data, bindingAdapterPosition)
+                onClickToNavigateToHomeDetail(data)
             }
+
+            initLikedBtnCLickListener(data)
+            initProfileBtnClickListener(data)
+            initKebabBtnClickListener(data)
+            initGhostBtnClickListener(data)
+        }
+    }
+
+    private fun initProfileBtnClickListener(data: FeedEntity) {
+        binding.ivHomeProfile.setOnClickListener {
+            onClickUserProfileBtn(data)
+        }
+    }
+
+    private fun initLikedBtnCLickListener(data: FeedEntity) {
+        with(binding) {
             btnHomeHeart.setOnClickListener {
                 data.contentId?.let { contentId ->
                     onClickLikedBtn(contentId, btnHomeHeart.isSelected)
@@ -54,32 +56,23 @@ class HomeViewHolder(
                     if (btnHomeHeart.isSelected) (likeNumber.toInt() - 1).toString() else (likeNumber.toInt() + 1).toString()
                 btnHomeHeart.isSelected = !btnHomeHeart.isSelected
             }
-            ivHomeProfile.setOnClickListener {
-                onClickUserProfileBtn(data, bindingAdapterPosition)
-            }
-
-            setTransparent(data)
         }
     }
 
-    private fun setTransparent(data: FeedEntity) {
-        binding.ivHomeGhostFillGreen.setOnClickListener {
-            if (!data.isGhost) {
-                onClickTransparentBtn(data, position)
-            } else {
-                onClickTransparentBtn(
-                    data,
-                    -2,
-                )
-            }
+    private fun initKebabBtnClickListener(data: FeedEntity) {
+        binding.btnHomeKebab.setOnClickListener {
+            onClickKebabBtn(data, bindingAdapterPosition)
         }
+    }
 
-        if (data.isGhost) {
-            binding.viewHomeTransparentBg.setBackgroundColor(Color.parseColor("#D9FCFCFD"))
-        } else {
-            val color = Transparent().calculateColorWithOpacity(data.memberGhost)
-            Timber.tag("color").d(color)
-            binding.viewHomeTransparentBg.setBackgroundColor(Color.parseColor(color))
+    private fun initGhostBtnClickListener(data: FeedEntity) {
+        binding.ivHomeGhostFillGreen.setOnClickListener {
+            onClickTransparentBtn(data)
         }
+    }
+
+    private fun setFeedTransparent(memberGhostPercent: Int) {
+        val color = Transparent().calculateColorWithOpacity(memberGhostPercent)
+        binding.viewHomeTransparentBg.setBackgroundColor(Color.parseColor(color))
     }
 }
