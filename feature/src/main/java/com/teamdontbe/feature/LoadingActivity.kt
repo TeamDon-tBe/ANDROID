1fc4ad221e217dc0502e578ce44ed0987a05bd87
Eonji-sw <ejkim0625@gmail.com>
2024-02-20 17:19:21 +0900

    #192 [FEAT] : 로딩뷰 생성

---
+++ feature/src/main/java/com/teamdontbe/feature/LoadingActivity.kt
@@ -0,0 +1,10 @@
+package com.teamdontbe.feature
+
+import com.teamdontbe.core_ui.base.BindingActivity
+import com.teamdontbe.feature.databinding.ActivityLoadingBinding
+
+class LoadingActivity : BindingActivity<ActivityLoadingBinding>(R.layout.activity_loading) {
+    override fun initView() {
+
+    }
+}
