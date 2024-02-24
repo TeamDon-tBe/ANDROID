fed178257329afeb16c32e7a09337e8c4ef0c1c6
Eonji-sw <ejkim0625@gmail.com>
2024-02-24 17:45:54 +0900

    #200 [MOD] : viewModel 불러오는 코드 수정

--- feature/src/main/java/com/teamdontbe/feature/dialog/DeleteWithTitleWideDialogFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/dialog/DeleteWithTitleWideDialogFragment.kt
@@ -4,21 +4,15 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
 import com.kakao.sdk.user.UserApiClient
 import com.teamdontbe.core_ui.base.BindingDialogFragment
 import com.teamdontbe.core_ui.util.context.dialogFragmentResize
 import com.teamdontbe.core_ui.util.intent.navigateTo
-import com.teamdontbe.core_ui.view.UiState
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.FragmentDeleteWithTitleWideDialogBinding
 import com.teamdontbe.feature.login.LoginActivity
-import com.teamdontbe.feature.mypage.MyPageViewModel
 import com.teamdontbe.feature.mypage.authwithdraw.MyPageAuthWithdrawViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -26,9 +20,8 @@ class DeleteWithTitleWideDialogFragment(
     private val title: String,
     private val content: String,
     private val typeIsLogout: Boolean,
-    private val selectedReason: String
+    private val selectedReason: String,
 ) : BindingDialogFragment<FragmentDeleteWithTitleWideDialogBinding>(R.layout.fragment_delete_with_title_wide_dialog) {
-    private val myPageViewModel by viewModels<MyPageViewModel>()
     private val withdrawViewModel by viewModels<MyPageAuthWithdrawViewModel>()
 
     override fun onViewCreated(
@@ -63,27 +56,16 @@ class DeleteWithTitleWideDialogFragment(
     private fun initDeleteButtonClick() {
         binding.btnDeleteWithTitleWideDialogDelete.setOnClickListener {
             when (typeIsLogout) {
-                true -> myPageViewModel.checkLogin(false)
+                true -> withdrawViewModel.checkLogin(false)
                 false -> {
-                    // 계정 삭제 로직 적기
-                    withdrawViewModel.deleteWithdraw.flowWithLifecycle(lifecycle).onEach {
-                        when (it) {
-                            is UiState.Loading -> Unit
-                            is UiState.Success -> {
-                                // 여기 추가하기
-                                UserApiClient.instance.unlink { error ->
-                                    if (error != null) {
-                                        Timber.e("연결 끊기 실패", error)
-                                    }
-                                    else {
-                                        Timber.i("연결 끊기 성공. SDK에서 토큰 삭제 됨")
-                                    }
-                                }
-                                withdrawViewModel.deleteWithdraw(selectedReason)
-                            }
-                            else -> Unit
+                    UserApiClient.instance.unlink { error ->
+                        if (error != null) {
+                            Timber.e("연결 끊기 실패", error)
+                        } else {
+                            Timber.i("연결 끊기 성공. SDK에서 토큰 삭제 됨")
+                            withdrawViewModel.deleteWithdraw(selectedReason)
                         }
-                    }.launchIn(lifecycleScope)
+                    }
                 }
             }
             navigateToLoginActivity()
