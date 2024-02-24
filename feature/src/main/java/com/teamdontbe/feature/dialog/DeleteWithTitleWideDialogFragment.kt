de68bbeb24f2b70c398e019d8798d260548c3b79
Eonji-sw <ejkim0625@gmail.com>
2024-02-24 18:01:22 +0900

    #200 [MOD] : observe 함수 추가

--- feature/src/main/java/com/teamdontbe/feature/dialog/DeleteWithTitleWideDialogFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/dialog/DeleteWithTitleWideDialogFragment.kt
@@ -1,18 +1,26 @@
 package com.teamdontbe.feature.dialog
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
 import com.kakao.sdk.user.UserApiClient
 import com.teamdontbe.core_ui.base.BindingDialogFragment
 import com.teamdontbe.core_ui.util.context.dialogFragmentResize
+import com.teamdontbe.core_ui.util.fragment.viewLifeCycle
+import com.teamdontbe.core_ui.util.fragment.viewLifeCycleScope
 import com.teamdontbe.core_ui.util.intent.navigateTo
+import com.teamdontbe.core_ui.view.UiState
+import com.teamdontbe.feature.ErrorActivity
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.FragmentDeleteWithTitleWideDialogBinding
 import com.teamdontbe.feature.login.LoginActivity
 import com.teamdontbe.feature.mypage.authwithdraw.MyPageAuthWithdrawViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import timber.log.Timber
 
 @AndroidEntryPoint
@@ -35,6 +43,7 @@ class DeleteWithTitleWideDialogFragment(
         initText()
         initCancelButtonClick()
         initDeleteButtonClick()
+        deleteObserve()
     }
 
     private fun initText() {
@@ -53,6 +62,16 @@ class DeleteWithTitleWideDialogFragment(
         }
     }
 
+    private fun deleteObserve() {
+        withdrawViewModel.deleteWithdraw.flowWithLifecycle(viewLifeCycle).onEach {
+            when (it) {
+                is UiState.Success -> Timber.tag("withdraw").i("계정 삭제 성공")
+                is UiState.Failure -> startActivity(Intent(context, ErrorActivity::class.java))
+                else -> Unit
+            }
+        }.launchIn(viewLifeCycleScope)
+    }
+
     private fun initDeleteButtonClick() {
         binding.btnDeleteWithTitleWideDialogDelete.setOnClickListener {
             when (typeIsLogout) {
@@ -68,6 +87,7 @@ class DeleteWithTitleWideDialogFragment(
                     }
                 }
             }
+
             navigateToLoginActivity()
             dismiss()
         }
