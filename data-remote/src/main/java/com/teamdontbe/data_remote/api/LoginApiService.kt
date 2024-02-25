d8b4ce2c248695f15044cad4aae93695ee4956d1
chanu <cw8881@naver.com>
2024-02-26 01:51:40 +0900

    #196 [add] : 이미지 멀티파트 api service함수 추가

--- data-remote/src/main/java/com/teamdontbe/data_remote/api/LoginApiService.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/api/LoginApiService.kt
@@ -4,10 +4,14 @@ import com.teamdontbe.data.dto.BaseResponse
 import com.teamdontbe.data.dto.request.RequestLoginDto
 import com.teamdontbe.data.dto.request.RequestProfileEditDto
 import com.teamdontbe.data.dto.response.ResponseLoginDto
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Query
 
 interface LoginApiService {
@@ -18,6 +22,8 @@ interface LoginApiService {
         const val NICKNAME_VALIDATION = "nickname-validation"
         const val NICKNAME = "nickname"
         const val USER_PROFILE = "user-profile"
+        const val USER_PROFILE_2 = "user-profile2"
+        const val INFO = "info"
     }
 
     @POST("/$API/$V1/$AUTH")
@@ -33,5 +39,12 @@ interface LoginApiService {
     @PATCH("/$API/$V1/$USER_PROFILE")
     suspend fun patchUserProfile(
         @Body requestProfileEdit: RequestProfileEditDto,
+    ): BaseResponse<Unit>
+
+    @Multipart
+    @PATCH("/$API/$V1/$USER_PROFILE_2")
+    suspend fun patchUserProfile2(
+        @Part(INFO) requestProfileEdit: RequestBody,
+        @Part file: MultipartBody.Part?,
     ): BaseResponse<Unit>
 }
