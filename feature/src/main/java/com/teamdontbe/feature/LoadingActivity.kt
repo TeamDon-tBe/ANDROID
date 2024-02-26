4fa0dd747d70ea65fecf76f6a2d1e841e0e0c3e2
Eonji-sw <ejkim0625@gmail.com>
2024-02-26 21:40:08 +0900

    #210 [MOD] : loadingActivity 애니메이션 적용 및 초 설정

--- feature/src/main/java/com/teamdontbe/feature/LoadingActivity.kt
+++ feature/src/main/java/com/teamdontbe/feature/LoadingActivity.kt
@@ -1,12 +1,39 @@
 package com.teamdontbe.feature
 
+import android.os.Handler
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.teamdontbe.core_ui.base.BindingActivity
 import com.teamdontbe.feature.databinding.ActivityLoadingBinding
 import kotlin.random.Random
 
 class LoadingActivity : BindingActivity<ActivityLoadingBinding>(R.layout.activity_loading) {
+
+    lateinit var fadeOutAnim: Animation
+
     override fun initView() {
         initLoadingContent()
+
+        initAnimation()
+    }
+
+    private fun initAnimation() {
+        Handler().postDelayed({
+            fadeOutAnim = AnimationUtils.loadAnimation(this, R.anim.anim_loading_fade_out)
+            fadeOutAnim.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    // 애니메이션이 끝난 후에 액티비티를 종료
+                    binding.layoutLoading.visibility = View.GONE
+                    finish()
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {}
+            })
+            binding.layoutLoading.startAnimation(fadeOutAnim)
+        }, 1800)
     }
 
     private fun initLoadingContent() {
