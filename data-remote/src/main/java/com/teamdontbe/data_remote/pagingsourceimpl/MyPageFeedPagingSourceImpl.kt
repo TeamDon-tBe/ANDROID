5959f174136febe354870bef6b7208e2dc4c6963
chanu <cw8881@naver.com>
2024-02-28 02:13:15 +0900

    #208 [mod] : MyPageFeedPagingSourceImpl 소현이가 준 걸로 수정

--- data-remote/src/main/java/com/teamdontbe/data_remote/pagingsourceimpl/MyPageFeedPagingSourceImpl.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/pagingsourceimpl/MyPageFeedPagingSourceImpl.kt
@@ -5,27 +5,44 @@ import androidx.paging.PagingState
 import com.teamdontbe.data_remote.api.MyPageApiService
 import com.teamdontbe.domain.entity.FeedEntity
 
-class MyPagePagingSourceImpl(
+class MyPageFeedPagingSourceImpl(
     private val myPageApiService: MyPageApiService,
     private val memberId: Int
 ) :
     PagingSource<Long, FeedEntity>() {
+
+    private var prevKey: Long? = null
+
+    companion object {
+        var refreshKey: MutableList<Pair<Long, Long?>> = mutableListOf()
+    }
+
     override fun getRefreshKey(state: PagingState<Long, FeedEntity>): Long? {
-        return state.anchorPosition?.let { anchorPosition ->
-            state.closestPageToPosition(anchorPosition)?.data?.last()?.contentId?.toLong() ?: -1
+        return state.anchorPosition?.let { position ->
+            prevKey = state.closestPageToPosition(position)?.prevKey
+            refreshKey.find { it.second == state.closestPageToPosition(position)?.prevKey }?.first
         }
     }
 
     override suspend fun load(params: LoadParams<Long>): LoadResult<Long, FeedEntity> {
         val position = params.key ?: -1
+        if (!refreshKey.any { it.first == position }) refreshKey.add(
+            Pair(
+                position,
+                prevKey
+            )
+        )
+
         return runCatching {
             val result = myPageApiService.getMyPageFeedList(memberId, position)
             LoadResult.Page(
                 // 매핑은 여기서
                 data = result.data?.map { it.toFeedEntity() } ?: emptyList(),
-                prevKey = if (position.toInt() == -1) null else result.data?.first()?.contentId?.toLong(),
+                prevKey = refreshKey.find { it.first == position }?.second,
                 nextKey = if (result.data.isNullOrEmpty()) null else result.data?.last()?.contentId?.toLong(),
-            )
+            ).also {
+                prevKey = position
+            }
         }.getOrElse {
             LoadResult.Error(it)
         }
