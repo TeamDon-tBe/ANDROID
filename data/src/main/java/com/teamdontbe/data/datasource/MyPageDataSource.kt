b0552ed20b9813ab5b3c79cb8b91711b7ff07de4
chanu <cw8881@naver.com>
2024-02-28 01:30:25 +0900

    #208 [mod] : 답글 dataSource 타입 flow 페이징으로 변경

--- data/src/main/java/com/teamdontbe/data/datasource/MyPageDataSource.kt
+++ data/src/main/java/com/teamdontbe/data/datasource/MyPageDataSource.kt
@@ -3,9 +3,9 @@ package com.teamdontbe.data.datasource
 import ResponseMyPageUserAccountInfoDto
 import androidx.paging.PagingData
 import com.teamdontbe.data.dto.BaseResponse
-import com.teamdontbe.data.dto.response.ResponseMyPageCommentDto
 import com.teamdontbe.data.dto.response.ResponseMyPageUserProfileDto
 import com.teamdontbe.domain.entity.FeedEntity
+import com.teamdontbe.domain.entity.MyPageCommentEntity
 import kotlinx.coroutines.flow.Flow
 
 interface MyPageDataSource {
@@ -13,7 +13,7 @@ interface MyPageDataSource {
 
     fun getMyPageUserFeedListSource(viewMemberId: Int): Flow<PagingData<FeedEntity>>
 
-    suspend fun getMyPageUserCommentListSource(viewMemberId: Int): BaseResponse<List<ResponseMyPageCommentDto>>
+    suspend fun getMyPageUserCommentListSource(viewMemberId: Int): Flow<PagingData<MyPageCommentEntity>>
 
     suspend fun getMyPageUserAccountInfo(): BaseResponse<ResponseMyPageUserAccountInfoDto>
 }
