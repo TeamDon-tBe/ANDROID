ba26a14ec85cf54aa55fa47772fc235f94859c70
Sohyun <so405@naver.com>
2024-02-27 20:44:41 +0900

    #195 [UI] : 온보딩 UI 작은 기기 대응

--- feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingFourthFragment.kt
+++ feature/src/main/java/com/teamdontbe/feature/onboarding/OnboardingFourthFragment.kt
@@ -1,8 +1,6 @@
 package com.teamdontbe.feature.onboarding
 
-import android.view.WindowManager
 import androidx.fragment.app.activityViewModels
-import coil.load
 import com.teamdontbe.core_ui.base.BindingFragment
 import com.teamdontbe.core_ui.util.context.hideKeyboard
 import com.teamdontbe.feature.R
@@ -18,20 +16,12 @@ class OnboardingFourthFragment() :
     override fun initView() {
         binding.vm = onboardingViewModel
         Timber.d("온보딩 네번째")
-        binding.tvOnboardingFourthNickname.text = onboardingViewModel.getNickName()
-        binding.ivOnboardingFourthProfile.load(onboardingViewModel.getUserProfile())
         hideKeyboard()
-        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
     }
 
     private fun hideKeyboard() {
         binding.root.setOnClickListener {
             requireContext().hideKeyboard(binding.root)
         }
-    }
-
-    override fun onDestroyView() {
-        requireActivity().window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
-        super.onDestroyView()
     }
 }
