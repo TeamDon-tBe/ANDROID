fe5882980e2c063cbcd86d213cdd9a96cd3f577c
chanu <cw8881@naver.com>
2024-02-27 20:24:55 +0900

    #208 [mod] : MyPageDataSourceImpl getMyPageUserFeedListSource 페이징 가능하도록 변경

--- data-remote/src/main/java/com/teamdontbe/data_remote/datasourceimpl/MyPageDataSourceImpl.kt
+++ data-remote/src/main/java/com/teamdontbe/data_remote/datasourceimpl/MyPageDataSourceImpl.kt
@@ -1,23 +1,30 @@
 package com.teamdontbe.data_remote.datasourceimpl
 
 import ResponseMyPageUserAccountInfoDto
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.teamdontbe.data.datasource.MyPageDataSource
 import com.teamdontbe.data.dto.BaseResponse
-import com.teamdontbe.data.dto.response.ResponseFeedDto
 import com.teamdontbe.data.dto.response.ResponseMyPageCommentDto
 import com.teamdontbe.data.dto.response.ResponseMyPageUserProfileDto
 import com.teamdontbe.data_remote.api.MyPageApiService
+import com.teamdontbe.data_remote.pagingsourceimpl.MyPagePagingSourceImpl
+import com.teamdontbe.domain.entity.FeedEntity
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class MyPageDataSourceImpl @Inject constructor(
     private val myPageApiService: MyPageApiService,
-) :
-    MyPageDataSource {
+) : MyPageDataSource {
     override suspend fun getMyPageUserProfileSource(viewMemberId: Int): BaseResponse<ResponseMyPageUserProfileDto> =
         myPageApiService.getMyPageUserProfile(viewMemberId)
 
-    override suspend fun getMyPageUserFeedListSource(viewMemberId: Int): BaseResponse<List<ResponseFeedDto>> =
-        myPageApiService.getMyPageFeedList(viewMemberId)
+    override fun getMyPageUserFeedListSource(viewMemberId: Int): Flow<PagingData<FeedEntity>> {
+        return Pager(PagingConfig(1)) {
+            MyPagePagingSourceImpl(myPageApiService, viewMemberId)
+        }.flow
+    }
 
     override suspend fun getMyPageUserCommentListSource(viewMemberId: Int): BaseResponse<List<ResponseMyPageCommentDto>> =
         myPageApiService.getMyPageCommentList(viewMemberId)
