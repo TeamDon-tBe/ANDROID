5d9b137cf9e0b616ee7557c63104f5fabfd08265
chanu <cw8881@naver.com>
2024-03-08 04:30:56 +0900

    #217 [hotfix] : qa반영 - 삭제,신고 바텀시트 올바른 아이콘으로 변경

--- feature/src/main/java/com/teamdontbe/feature/mypage/bottomsheet/MyPageAnotherUserBottomSheet.kt
+++ feature/src/main/java/com/teamdontbe/feature/mypage/bottomsheet/MyPageAnotherUserBottomSheet.kt
@@ -1,5 +1,6 @@
 package com.teamdontbe.feature.mypage.bottomsheet
 
+import android.view.View
 import com.teamdontbe.core_ui.base.BindingBottomSheetFragment
 import com.teamdontbe.feature.R
 import com.teamdontbe.feature.databinding.BottomsheetComplaintDeleteBinding
@@ -9,39 +10,46 @@ class MyPageAnotherUserBottomSheet(
     private val contentId: Int,
     private val commentId: Int,
     private val whereFrom: String,
-) :
-    BindingBottomSheetFragment<BottomsheetComplaintDeleteBinding>(R.layout.bottomsheet_complaint_delete) {
+) : BindingBottomSheetFragment<BottomsheetComplaintDeleteBinding>(R.layout.bottomsheet_complaint_delete) {
+
     override fun initView() {
-        initBottomSheetCloseClickListener()
-        initShowDialogClickListener()
         initText()
+        initClickListener()
     }
 
     private fun initText() {
-        if (isMember) {
-            binding.tvComplaintTitle.text = getString(R.string.tv_delete_title)
-        }
+        val complaintTitleVisibility = if (isMember) View.GONE else View.VISIBLE
+        val deleteTitleVisibility = if (isMember) View.VISIBLE else View.GONE
+
+        binding.tvComplaintTitle.visibility = complaintTitleVisibility
+        binding.tvDeleteTitle.visibility = deleteTitleVisibility
     }
 
-    private fun initBottomSheetCloseClickListener() {
+    private fun initClickListener() {
         binding.ivComplaintDeleteClose.setOnClickListener {
             dismiss()
         }
+
+        binding.tvComplaintTitle.setOnClickListener {
+            showDialog()
+        }
+
+        binding.tvDeleteTitle.setOnClickListener {
+            showDialog()
+        }
     }
 
-    private fun initShowDialogClickListener() {
-        binding.tvComplaintTitle.setOnClickListener {
-            MyPageDeleteDialogFragment(
-                isMember,
-                contentId,
-                commentId,
-                whereFrom,
-            ).show(
-                parentFragmentManager,
-                DECLARE_DIALOG,
-            )
-            dismiss()
-        }
+    private fun showDialog() {
+        MyPageDeleteDialogFragment(
+            isMember,
+            contentId,
+            commentId,
+            whereFrom,
+        ).show(
+            parentFragmentManager,
+            DECLARE_DIALOG,
+        )
+        dismiss()
     }
 
     companion object {
