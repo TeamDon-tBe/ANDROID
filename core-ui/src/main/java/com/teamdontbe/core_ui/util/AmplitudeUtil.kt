297f02605048966adeacc4cc4291e8f3211e6d0f
Sohyun <so405@naver.com>
2024-03-26 19:09:09 +0900

    #245 [FEAT] : 엠플리튜드 자동 추적 기능 키기(디폴트가 all임)

--- core-ui/src/main/java/com/teamdontbe/core_ui/util/AmplitudeUtil.kt
+++ core-ui/src/main/java/com/teamdontbe/core_ui/util/AmplitudeUtil.kt
@@ -3,7 +3,6 @@ package com.teamdontbe.core_ui.util
 import android.content.Context
 import com.amplitude.android.Amplitude
 import com.amplitude.android.Configuration
-import com.amplitude.android.DefaultTrackingOptions
 import com.teamdontbe.core_ui.BuildConfig.AMPLITUDE_API_KEY
 
 object AmplitudeUtil {
@@ -14,7 +13,6 @@ object AmplitudeUtil {
             Configuration(
                 apiKey = AMPLITUDE_API_KEY,
                 context = applicationContext,
-                defaultTracking = DefaultTrackingOptions.NONE
             )
         )
     }
