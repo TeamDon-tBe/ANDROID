6dba8419f548c0ef222acb4557eae01311f79ac1
Sohyun <so405@naver.com>
2024-04-25 22:01:01 +0900

    #247 [FEAT] : 노티 프로필 클릭 이벤트

--- feature/src/main/java/com/teamdontbe/feature/notification/adapter/NotificationViewHolder.kt
+++ feature/src/main/java/com/teamdontbe/feature/notification/adapter/NotificationViewHolder.kt
@@ -1,8 +1,13 @@
 package com.teamdontbe.feature.notification.adapter
 
+import android.graphics.Color
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.style.ClickableSpan
 import android.text.style.StyleSpan
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.teamdontbe.domain.entity.NotiEntity
 import com.teamdontbe.feature.R
@@ -13,66 +18,66 @@ import timber.log.Timber
 class NotificationViewHolder(
     private val binding: ItemNotificationFeedBinding,
     private val click: (NotiEntity, Int) -> Unit,
+    private val onClickUserProfileBtn: (Int) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(data: NotiEntity) {
         with(binding) {
-            val spannableText =
-                when (data.notificationTriggerType) {
-                    "contentLiked" ->
-                        getSpannableText(
-                            data.triggerMemberNickname,
-                            R.string.notification_feed_content_liked,
-                        )
-
-                    "comment" ->
-                        getSpannableText(
-                            data.triggerMemberNickname,
-                            R.string.notification_feed_comment,
-                        )
-
-                    "commentLiked" ->
-                        getSpannableText(
-                            data.triggerMemberNickname,
-                            R.string.notification_feed_comment_liked,
-                        )
-
-                    "actingContinue" ->
-                        getSpannableText(
-                            data.memberNickname,
-                            R.string.notification_feed_acting_continue,
-                        )
-
-                    "beGhost" ->
-                        getSpannableText(
-                            data.memberNickname,
-                            R.string.notification_feed_be_ghost,
-                        )
-
-                    "contentGhost" ->
-                        getSpannableText(
-                            data.memberNickname,
-                            R.string.notification_feed_content_ghost,
-                        )
-
-                    "commentGhost" ->
-                        getSpannableText(
-                            data.memberNickname,
-                            R.string.notification_feed_comment_ghost,
-                        )
-
-                    "userBan" ->
-                        getSpannableText(
-                            data.memberNickname,
-                            R.string.notification_feed_user_ban,
-                            36,
-                        )
-
-                    else -> {
-                        Timber.tag("noti")
-                            .e("등록되지 않은 노티가 감지되었습니다 : ${data.notificationTriggerType}")
-                        SpannableString("")
-                    }
+            val spannableText = when (data.notificationTriggerType) {
+                "contentLiked" -> {
+                    initProfileBtnClickListener(data)
+                    getSpannableText(
+                        R.string.notification_feed_content_liked,
+                        data = data
+                    )
                 }
+
+                "comment" -> {
+                    initProfileBtnClickListener(data)
+                    getSpannableText(
+                        R.string.notification_feed_comment,
+                        data = data
+                    )
+                }
+
+                "commentLiked" -> {
+                    initProfileBtnClickListener(data)
+                    getSpannableText(
+                        R.string.notification_feed_comment_liked,
+                        data = data
+                    )
+                }
+
+                "actingContinue" -> getSpannableText(
+                    R.string.notification_feed_acting_continue,
+                    data = data
+                )
+
+                "beGhost" -> getSpannableText(
+                    R.string.notification_feed_be_ghost,
+                    data = data
+                )
+
+                "contentGhost" -> getSpannableText(
+                    R.string.notification_feed_content_ghost,
+                    data = data
+                )
+
+                "commentGhost" -> getSpannableText(
+                    R.string.notification_feed_comment_ghost,
+                    data = data
+                )
+
+                "userBan" -> getSpannableText(
+                    R.string.notification_feed_user_ban,
+                    36,
+                    data = data
+                )
+
+                else -> {
+                    Timber.tag("noti").e("등록되지 않은 노티가 감지되었습니다 : ${data.notificationTriggerType}")
+                    SpannableString("")
+                }
+            }
 
             tvNotificationFeed.text = spannableText
             val timeUtil = CalculateTime(binding.root.context)
@@ -87,19 +92,44 @@ class NotificationViewHolder(
         }
     }
 
+    private fun initProfileBtnClickListener(data: NotiEntity) {
+        binding.ivNotificationProfile.setOnClickListener {
+            if (!data.isDeleted) onClickUserProfileBtn(data.triggerMemberId)
+        }
+    }
+
     private fun getSpannableText(
-        name: String,
         resId: Int,
         endIndex: Int = 0,
-    ): SpannableString {
-        val text = name + binding.root.context.getString(resId)
-        val spannableText = SpannableString(text)
+        data: NotiEntity,
+    ): SpannableStringBuilder {
+        val text = data.memberNickname + binding.root.context.getString(resId)
+        val spannableText = SpannableStringBuilder(text)
+        if (data.notificationTriggerType == "contentLiked" || data.notificationTriggerType == "comment" || data.notificationTriggerType == "commentLiked") {
+            spannableText.setSpan(
+                clickableSpan(data),
+                0,
+                data.memberNickname.length + endIndex,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
         spannableText.setSpan(
             StyleSpan(R.font.font_pretendard_semibold),
             0,
-            name.length + endIndex,
+            data.memberNickname.length + endIndex,
             Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
         )
         return spannableText
+    }
+
+    private fun clickableSpan(data: NotiEntity) = object : ClickableSpan() {
+        override fun onClick(view: View) {
+            if (!data.isDeleted) onClickUserProfileBtn(data.triggerMemberId)
+        }
+
+        override fun updateDrawState(ds: TextPaint) {
+            ds.isUnderlineText = false
+            ds.color = Color.parseColor("#FF49494C")
+        }
     }
 }
