9ab2780a8b2b1d2148426e7195224a1e43ba0693
Sohyun <so405@naver.com>
2024-05-03 16:35:54 +0900

    #253 [FEAT] : Link error 커스텀 스낵바

---
+++ feature/src/main/java/com/teamdontbe/feature/snackbar/LinkCountErrorSnackBar.kt
@@ -0,0 +1,40 @@
+package com.teamdontbe.feature.snackbar
+
+import android.view.LayoutInflater
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
+import com.teamdontbe.feature.R
+import com.teamdontbe.feature.databinding.SnackbarLinkCountErrorBinding
+
+class LinkCountErrorSnackBar(view: View) {
+    companion object {
+        fun make(view: View) = LinkCountErrorSnackBar(view)
+    }
+
+    private val context = view.context
+    private val snackbar = Snackbar.make(view, "", 1200)
+    private val snackbarLayout = snackbar.view as? Snackbar.SnackbarLayout
+
+    private val inflater = LayoutInflater.from(context)
+    private val binding: SnackbarLinkCountErrorBinding =
+        DataBindingUtil.inflate(inflater, R.layout.snackbar_link_count_error, null, false)
+
+    init {
+        initView()
+    }
+
+    private fun initView() {
+        snackbarLayout?.run {
+            removeAllViews()
+            setPadding(0, 0, 0, 0)
+            setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent))
+            addView(binding.root, 0)
+        }
+    }
+
+    fun show() {
+        snackbar.show()
+    }
+}
